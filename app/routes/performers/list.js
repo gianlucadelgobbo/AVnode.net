cc8afc9e8af78e42cc17ab51a3c53dfa12b96b7a
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-01-11 16:42:27 +0100

    File rearrange + Performers lists with filters

--- app/routes/performers/list.js
+++ app/routes/performers/list.js
@@ -1,22 +1,98 @@
+const config = require('getconfig');
 const router = require('../router')();
 const logger = require('../../utilities/logger');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
+
+const dataprovider = require('../../utilities/dataprovider');
+
+router.get('/:filter/:sorting/:page', (req, res) => {
+  logger.debug('LIST page');
+  list(req, res);
+});
+
+router.get('/:filter/:sorting', (req, res) => {
+  logger.debug('LIST sorting');
+  req.params.page = 1;
+  list(req, res);
+});
+
+router.get('/:filter', (req, res) => {
+  logger.debug('LIST filter');
+  req.params.page = 1;
+  req.params.sorting = config.sections[section].orders[0];
+  list(req, res);
+});
 
 router.get('/', (req, res) => {
   logger.debug('LIST');
-  User.find({})
-  .populate([{
-    path: 'image',
-    model: 'Asset'
-  }])
-  .limit(5)
-  .exec((err, data) => {
-    res.render('performers/list', {
-      title: __('Performers'),
-      data: data
-    });
-  });
+  req.params.page = 1;
+  req.params.sorting = config.sections[section].orders[0];
+  req.params.filter = config.sections[section].categories[0];
+  list(req, res);
 });
+
+list = (req, res) => {
+  logger.debug('LIST LIST');
+  var section = 'performers';
+  
+  var page = req.params.page;
+  var filter = req.params.filter;
+  var sorting = req.params.sorting;
+
+  var notfound = false;
+  if (config.sections[section].categories.indexOf(filter) === -1) notfound = true;
+  if (typeof config.sections[section].sortQ[sorting]=="undefined") notfound = true;
+  if (parseInt(page).toString()!=page.toString()) notfound = true;
+    
+  var skip = (page - 1) * config.sections[section].limit;
+  
+  
+  if (notfound) {
+    res.status(404).render('404', {
+      title: __('Hmm…'),
+      nav: [],
+      path: req.originalUrl
+    });
+  } else {
+    var path = '/' + section + '/' + filter + '/' + sorting + '/' + page;
+    //path = path.replace('//', '/');
+    // TODO: Validate that the params above are configured in `config`, if not
+    // => 404 or 500
+    var query = filter=='individuals' ? {is_crew: 0} : filter=='crews' ? {is_crew: 1} : {};
+  
+    dataprovider.fetchPerformers(query, config.sections[section].limit, skip, config.sections[section].sortQ[sorting], (err, data, total) => {
+      logger.debug('bella');
+      var title = config.sections[section].title;
+      var info = ' From ' + skip + ' to ' + (skip + config.sections[section].limit) + ' on ' + total + ' ' + title;
+      var link = '/' + section + '/' + filter + '/' + sorting + '/';
+      //var pages = _h.pagination(link, skip, config.sections[section].limit, total);
+      res.render('performers/list', {
+        title: __('Performers'),
+        section: section,
+        sort: sorting,
+        filter: filter,
+        categories: config.sections[section].categories,
+        orderings: config.sections[section].orders,
+       data: data
+      });
+      /*res.render(section + '/list', {
+        title: title,
+        info: info,
+        section: section,
+        total: total,
+        path: path,
+        sort: sorting,
+        filter: filter,
+        skip: skip,
+        page: page,
+        pages: pages,
+        result: events,
+        categories: config.sections[section].categories,
+        orderings: config.sections[section].orders,
+        user: req.user,
+        _h: _h
+      });*/
+    });
+  }
+}
 
 module.exports = router;
