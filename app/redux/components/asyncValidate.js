82593df799ad489f999625b2efaeb321f8e00004
piero80 <pcolangelo80@gmail.com>
2018-02-12 22:05:00 +0100

    Change validators

--- app/redux/components/asyncValidate.js
+++ app/redux/components/asyncValidate.js
@@ -1,12 +1,12 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 import { fetchSlug } from '../reducers/actions';
 const asyncValidate = (values, dispatch ) => {
-  return dispatch(fetchSlug(values.slug, dispatch)).then((response) => {
-    if(response.payload.exist) {
-      throw { slug: 'That slug is taken' }
-    }
-  });
-}
+    return dispatch(fetchSlug(values.slug, dispatch)).then((response) => {
+      if(response.payload.exist) {
+        throw { slug: 'That slug is taken' }
+      }
+    });
+  }
 
 export default asyncValidate
 
