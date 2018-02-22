8c6ed30c2717173fa286a7c85748f32e1d9025d0
donostiastudio <donostiasocialclub@gmail.com>
2018-02-22 19:37:45 +0100

    performer design - google maps

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
+ //http://kenwheeler.github.io/slick/ 
     $('.carousel').slick({
       centerMode: true,
       centerPadding: '80px',
       slidesToShow: 3,
-      arrows: false
+      arrows: true
     });
   });  
