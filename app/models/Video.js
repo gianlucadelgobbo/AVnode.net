be7f6b0526dd14f837b86e2df8def6f4380710bd
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-02-24 01:38:31 +0100

    Image generator

--- app/models/Video.js
+++ app/models/Video.js
@@ -43,8 +43,8 @@ videoSchema.virtual('imageFormats').get(function () {
   let imageFormats = {};
   //console.log(config.cpanel[adminsez].sizes.image);
   if (this.media && this.media.file) {
-    for(let format in config.cpanel[adminsez].media.image.sizes) {
-      imageFormats[format] = config.cpanel[adminsez].media.image.sizes[format].default;
+    for(let format in config.cpanel[adminsez].media.media.sizes) {
+      imageFormats[format] = config.cpanel[adminsez].media.media.sizes[format].default;
     }
     const serverPath = this.media.file;
     const localFileName = serverPath.substring(serverPath.lastIndexOf('/') + 1); // file.jpg this.file.file.substr(19)
@@ -53,12 +53,12 @@ videoSchema.virtual('imageFormats').get(function () {
     const localFileNameWithoutExtension = localFileName.substring(0, localFileName.lastIndexOf('.'));
     const localFileNameExtension = localFileName.substring(localFileName.lastIndexOf('.') + 1);
     // console.log('localFileName:' + localFileName + ' localPath:' + localPath + ' localFileNameWithoutExtension:' + localFileNameWithoutExtension);
-    for(let format in config.cpanel[adminsez].media.image.sizes) {
-      imageFormats[format] = `${localPath}/${config.cpanel[adminsez].media.image.sizes[format].folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
+    for(let format in config.cpanel[adminsez].media.media.sizes) {
+      imageFormats[format] = `${localPath}/${config.cpanel[adminsez].media.media.sizes[format].folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
     }
   } else {
-    for(let format in config.cpanel[adminsez].media.image.sizes) {
-      imageFormats[format] = `${config.cpanel[adminsez].media.image.sizes[format].default}`;
+    for(let format in config.cpanel[adminsez].media.media.sizes) {
+      imageFormats[format] = `${config.cpanel[adminsez].media.media.sizes[format].default}`;
     }
   }
   return imageFormats;
