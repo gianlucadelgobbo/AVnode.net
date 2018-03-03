c23f433c1f297d31eae2e728fd2a84082c90726b
Daniele Salvatore <danieleslvtr@gmail.com>
2018-03-03 18:11:18 +0100

    Profile public form

--- app/redux/components/about/Abouts.js
+++ app/redux/components/about/Abouts.js
@@ -1,72 +1,73 @@
-import { h } from 'preact';
-import { Field } from 'redux-form';
-import { injectIntl, FormattedMessage } from 'preact-intl';
+import {h} from 'preact';
+import {Field} from 'redux-form';
+import {injectIntl, FormattedMessage} from 'preact-intl';
 import renderLabel from '../renderLabel';
 import Languages from '../language/Languages';
-import { Tab, Tabs, Row, Col, Nav, NavItem  } from 'react-bootstrap';
+import {Tab, Tabs, Row, Col, Nav, NavItem} from 'react-bootstrap';
 
 const Abouts = injectIntl(({
-  fields,
-  meta: { error, submitFailed },
-  intl
-}) => {
+                               fields,
+                               meta: {error, submitFailed},
+                               intl
+                           }) => {
 
-  return (
-    
-  <fieldset>
-    <div>
-      <label>
-        <FormattedMessage
-          id="abouts"
-          defaultMessage="About you..."
-        />
-      </label>
-      {submitFailed && error && <span>{error}</span>}
+        return (
 
-      <Tab.Container id="left-tab-languages" defaultActiveKey={0}>
-      <Row className="clearfix">
-        <Col sm={2} className="navabout">
-            <Nav bsStyle="pills" stacked>
-            {Languages.map((lang,index) => (
-                <NavItem eventKey={index}>{lang.language}</NavItem>
-            ))}
-            </Nav>
-        </Col>
-        <Col sm={10}>
-        <Tab.Content animation>
-        {fields.map((about, index) => (
-        <Tab.Pane eventKey={index}>
-        <div key={index}>
-          <label htmlFor="aboutlanguage">
-            <FormattedMessage
-              id="aboutTitle"
-              defaultMessage="About section in language:"
-            />
-          </label>
-          <Field
-            className="form-control"
-            name={`${about}.lang`}
-            component={renderLabel}
-          />
-        </div>
-        <Field
-          className="form-control"
-          name={`${about}.abouttext`}
-          component="textarea"      
-          rows="12"
-          placeholder={intl.formatMessage({
-            id: 'about.placeholder',
-            defaultMessage: 'Tell me something about you'
-          })}
-        />
-        </Tab.Pane>
-      ))}
-      </Tab.Content>
-      </Col>
-      </Row>
-  </Tab.Container>
-    </div>
-    </fieldset>
-  );}
+            <fieldset>
+                <div>
+                    <label>
+                        <FormattedMessage
+                            id="abouts"
+                            defaultMessage="About you..."
+                        />
+                    </label>
+                    {submitFailed && error && <span>{error}</span>}
+
+                    <Tab.Container id="left-tab-languages" defaultActiveKey={0}>
+                        <Row className="clearfix">
+                            <Col sm={2} className="navabout">
+                                <Nav bsStyle="pills" stacked>
+                                    {Languages.map((lang, index) => (
+                                        <NavItem eventKey={index}>{lang.language}</NavItem>
+                                    ))}
+                                </Nav>
+                            </Col>
+                            <Col sm={10}>
+                                <Tab.Content animation>
+                                    {fields.map((about, index) => (
+                                        <Tab.Pane eventKey={index}>
+                                            <div key={index}>
+                                                <label htmlFor="aboutlanguage">
+                                                    <FormattedMessage
+                                                        id="aboutTitle"
+                                                        defaultMessage="About section in language:"
+                                                    />
+                                                </label>
+                                                <Field
+                                                    className="form-control"
+                                                    name={`${about}.lang`}
+                                                    component={renderLabel}
+                                                />
+                                            </div>
+                                            <Field
+                                                className="form-control"
+                                                name={`${about}.abouttext`}
+                                                component="textarea"
+                                                rows="12"
+                                                placeholder={intl.formatMessage({
+                                                    id: 'about.placeholder',
+                                                    defaultMessage: 'Tell me something about you'
+                                                })}
+                                            />
+                                        </Tab.Pane>
+                                    ))}
+                                </Tab.Content>
+                            </Col>
+                        </Row>
+                    </Tab.Container>
+                </div>
+            </fieldset>
+        );
+    }
 );
 export default Abouts;
--- app/redux/components/common/form/components.js
+++ app/redux/components/common/form/components.js
@@ -1,5 +1,7 @@
 import {h} from 'preact';
 import Textarea from 'react-textarea-autosize';
+import {Tab, Tabs, Row, Col, Nav, NavItem} from 'react-bootstrap';
+import {Field} from "redux-form";
 
 export const inputText = ({input, meta, placeholder}) => {
     return <div className="form-group">
@@ -17,9 +19,42 @@ export const inputPassword = ({input, meta, placeholder}) =>
         {meta.error && meta.touched && <span className="error-message">{meta.error}</span>}
     </div>;
 
-export const textarea = ({input, id, meta, placeholder, options}) =>
+export const inputUrl = ({input, meta, placeholder}) =>
     <div className="form-group">
         <label htmlFor="first_name">{placeholder}</label>
+        <input type="url" className="form-control" {...input} placeholder={placeholder}/>
+        {meta.error && meta.touched && <span className="error-message">{meta.error}</span>}
+    </div>;
+
+export const textarea = ({input, id, meta, placeholder, options}) =>
+    <div className="form-group">
+        {placeholder && <label htmlFor="first_name">{placeholder}</label>}
         <Textarea id={id} className="form-control" {...input} placeholder={placeholder}/>
         {meta.error && meta.touched && <span>{meta.error}</span>}
     </div>;
+
+export const textareaMultitab = ({tabs = [], name, labels = {}, fields}) => {
+    return <Tab.Container id="left-tab-languages" defaultActiveKey={0}>
+        <Row className="clearfix">
+            <Col sm={2} className="navabout">
+                <Nav bsStyle="pills" stacked>
+                    {tabs.map((k, index) => (
+                        <NavItem eventKey={index}>{labels[k]}</NavItem>
+                    ))}
+                </Nav>
+            </Col>
+            <Col sm={10}>
+                <Tab.Content animation>
+                    {fields.map((member, index) => (
+                        <Tab.Pane eventKey={index}>
+                            <Field
+                                name={`${member}.value`}
+                                component={textarea}
+                            />
+                        </Tab.Pane>
+                    ))}
+                </Tab.Content>
+            </Col>
+        </Row>
+    </Tab.Container>;
+};
