288df69b2cd24de021d726814e8b796b0a7f2081
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-03-27 20:02:50 +0200

    API Refactor

--- app/models/Event.js
+++ app/models/Event.js
@@ -11,7 +11,7 @@ const Venue = require('./shared/Venue');
 //const Schedule = require('./shared/Schedule');
 //const Package = require('./shared/Package');
 
-const adminsez = 'event';
+const adminsez = 'events';
 const logger = require('../utilities/logger');
 
 const scheduleSchema = new Schema({
@@ -219,8 +219,8 @@ eventSchema.virtual('imageFormats').get(function () {
   let imageFormats = {};
   //console.log(config.cpanel[adminsez].sizes.image);
   if (this.image && this.image.file) {
-    for(let format in config.cpanel[adminsez].media.image.sizes) {
-      imageFormats[format] = config.cpanel[adminsez].media.image.sizes[format].default;
+    for(let format in config.cpanel[adminsez].forms.image.components.image.config.sizes) {
+      imageFormats[format] = config.cpanel[adminsez].forms.image.components.image.config.sizes[format].default;
     }
     const serverPath = this.image.file;
     const localFileName = serverPath.substring(serverPath.lastIndexOf('/') + 1); // file.jpg this.file.file.substr(19)
@@ -228,12 +228,12 @@ eventSchema.virtual('imageFormats').get(function () {
     const localFileNameWithoutExtension = localFileName.substring(0, localFileName.lastIndexOf('.'));
     const localFileNameExtension = localFileName.substring(localFileName.lastIndexOf('.') + 1);
     // console.log('localFileName:' + localFileName + ' localPath:' + localPath + ' localFileNameWithoutExtension:' + localFileNameWithoutExtension);
-    for(let format in config.cpanel[adminsez].media.image.sizes) {
-      imageFormats[format] = `${localPath}/${config.cpanel[adminsez].media.image.sizes[format].folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
+    for(let format in config.cpanel[adminsez].forms.image.components.image.config.sizes) {
+      imageFormats[format] = `${localPath}/${config.cpanel[adminsez].forms.image.components.image.config.sizes[format].folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
     }
   } else {
-    for(let format in config.cpanel[adminsez].media.image.sizes) {
-      imageFormats[format] = `${config.cpanel[adminsez].media.image.sizes[format].default}`;
+    for(let format in config.cpanel[adminsez].forms.image.components.image.config.sizes) {
+      imageFormats[format] = `${config.cpanel[adminsez].forms.image.components.image.config.sizes[format].default}`;
     }
   }
   return imageFormats;
---
+++ app/routes/admin/res.js
@@ -0,0 +1,29 @@
+// SUCCESS
+{
+  errors: false,
+  res: RETURNED_RES // Probably null
+}
+
+// ERRORS 404
+{
+  errors: {
+    content:['404']
+  },
+  res: null
+}
+
+// ERRORS 500
+{
+  errors: {
+    content:['DOC_NOT_FOUND']
+  },
+  res: null
+}
+
+{
+  errors:{
+    FIELDNAME_1:['CODE_HR'],  // Errors on fields
+    FIELDNAME_2:['CODE_HR'] // Errors on fields
+  },
+  res: RETURNED_RES // Probably null
+}
