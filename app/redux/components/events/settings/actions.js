cba3fce19bbb77da0827ac15fa1d644f6f7e1743
Daniele Salvatore <danieleslvtr@gmail.com>
2018-03-31 18:42:03 +0200

    events/settings

---
+++ app/redux/components/events/settings/actions.js
@@ -0,0 +1,51 @@
+import * as api from '../../../api';
+import {normalize} from 'normalizr';
+import {FETCH_MODEL_REQUEST, FETCH_MODEL_SUCCESS, FETCH_MODEL_ERROR} from '../constants'
+import {SAVE_MODEL_REQUEST, SAVE_MODEL_SUCCESS, SAVE_MODEL_ERROR} from '../constants'
+import {event} from '../../../schemas/event'
+
+export const fetchModel = ({id} = {}) => (dispatch) => {
+
+    dispatch({
+        type: FETCH_MODEL_REQUEST,
+        id
+    });
+
+    return api.fetchEventSettings({id})
+        .then(
+            (response) => {
+                dispatch({
+                    type: FETCH_MODEL_SUCCESS,
+                    response: normalize(response || [], event)
+                });
+            },
+            (error) => {
+                dispatch({
+                    type: FETCH_MODEL_ERROR,
+                    errorMessage: error.message || 'Something went wrong.'
+                });
+            });
+};
+
+export const saveModel = (model) => (dispatch) => {
+
+    dispatch({
+        type: SAVE_MODEL_REQUEST,
+        id: model.id
+    });
+
+    return api.saveEventSettings(model)
+        .then(
+            (response) => {
+                dispatch({
+                    type: SAVE_MODEL_SUCCESS,
+                    response: normalize(response || [], event)
+                });
+            },
+            (error) => {
+                dispatch({
+                    type: SAVE_MODEL_ERROR,
+                    errorMessage: error.message || 'Something went wrong.'
+                });
+            });
+};
