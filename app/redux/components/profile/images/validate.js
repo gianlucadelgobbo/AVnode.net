06dad2a6b1aa28d9e3783f8497f8eda70854d088
Daniele Salvatore <danieleslvtr@gmail.com>
2018-04-14 10:07:19 +0200

    profile/images

--- app/redux/components/profile/images/validate.js
+++ app/redux/components/profile/images/validate.js
@@ -1,3 +1,5 @@
+import {UPLOAD_IMAGE_MAX_SIZE} from '../../../conf'
+
 const validate = values => {
 
     const errors = {};
@@ -9,6 +11,14 @@ const validate = values => {
 
     if (Array.isArray(images) && images.length > 1) {
         errors.images = "Invalid amount of images (only 1)";
+    }
+
+    if (Array.isArray(images) && images.length === 1) {
+        const image = images[0];
+
+        if (image.size > UPLOAD_IMAGE_MAX_SIZE) {
+            errors.images = "Invalid image size";
+        }
     }
 
     return errors
