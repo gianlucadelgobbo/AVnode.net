0a8584280ff961659ac300f851f664b02a351b4c
Daniele Salvatore <danieleslvtr@gmail.com>
2018-05-26 18:31:07 +0200

    Add validation Add Partner Form

--- app/redux/components/modal/partners/add/validate.js
+++ app/redux/components/modal/partners/add/validate.js
@@ -1,7 +1,15 @@
-
 const validate = values => {
 
     const errors = {};
+    const {partner, category} = values;
+
+    if (!partner || !partner._id) {
+        errors.partner = "Mandatory"
+    }
+
+    if (!category || !category.value) {
+        errors.category = "Mandatory"
+    }
 
     return errors
 };
