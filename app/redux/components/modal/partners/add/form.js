4b351f157b758b1bbf6a8c22cec02e1b1293f1d0
Daniele Salvatore <danieleslvtr@gmail.com>
2018-05-26 18:11:52 +0200

    Partner component

---
+++ app/redux/components/modal/partners/add/form.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import {reduxForm, Field} from "redux-form";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {FORM_NAME} from './constants'
+import {userAutocompleteSelect} from "../../../common/form/components";
+import validate from './validate'
+import asyncValidate from './asyncValidate'
+
+class AddPartnerForm extends Component {
+
+    submitForm(data) {
+        const {onSubmit, reset} = this.props;
+
+        // reset form after submit
+        return onSubmit(data)
+            .then(() => {
+                reset();
+            });
+    }
+
+    render() {
+
+        const {
+            submitting,
+            handleSubmit
+        } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.submitForm.bind(this))}>
+
+                <Field
+                    name="images"
+                    component={userAutocompleteSelect}
+                    placeholder="Partner"
+                />
+
+                <hr/>
+
+                <button
+                    type="submit"
+                    disabled={submitting}
+                    className="btn btn-primary btn-lg btn-block">
+                    {submitting ? "Saving..." : "Save"}
+                </button>
+
+            </form>
+        );
+    }
+
+}
+
+
+//Get form's initial values from redux state here
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+
+AddPartnerForm = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AddPartnerForm);
+
+AddPartnerForm = reduxForm({
+    form: FORM_NAME,
+    enableReinitialize: true,
+    keepDirtyOnReinitialize: true,
+    validate,
+    asyncValidate,
+    //asyncBlurFields: ['slug', 'addresses']
+})(AddPartnerForm);
+
+export default AddPartnerForm;
