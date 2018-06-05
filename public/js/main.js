ac22b95e1c95f405e58920bb2fc8f4367aa465a8
donostiastudio <donostiasocialclub@gmail.com>
2018-06-05 13:33:02 +0200

    smooth scroll and bac to top UX function

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
       $( ".event_main_image_wrapper" ).toggleClass("expanded");
     });
 
+    
     $('.loop_2').owlCarousel({
       center: true,
       items:2,
@@ -68,5 +69,54 @@ $(document).ready(function(){
 
         }
     });
-    
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 800) {
+        $("#to_top").addClass("to_top_visibile");
+    } else {
+        $("#to_top").removeClass("to_top_visibile");
+    }
+});  
+
+
   });  
