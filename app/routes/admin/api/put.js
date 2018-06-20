798f96ca1cca89e4b139cff72ed8d8fd814c7269
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-06-20 14:04:19 +0200

    Validation

--- app/routes/admin/api/put.js
+++ app/routes/admin/api/put.js
@@ -31,6 +31,7 @@ router.putData = (req, res) => {
           data.save((err) => {
             if (err) {
               console.log('err');
+              console.log(err);
               res.status(400).json(err);
             } else {
               select = Object.assign(config.cpanel[req.params.sez].forms[req.params.form].select, config.cpanel[req.params.sez].forms[req.params.form].selectaddon);
