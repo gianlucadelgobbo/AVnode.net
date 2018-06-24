654e8059102bd7c4d1b89ee2b2aed2bbc52d28a5
piero80 <pcolangelo80@gmail.com>
2018-06-24 19:18:52 +0200

    Validation Error Private Profile

--- app/redux/components/common/form/errors.js
+++ app/redux/components/common/form/errors.js
@@ -9,7 +9,10 @@ export const INVALID_ADDRESS = "validation.error.invalid.address";
 export const INVALID_STRING_LENGTH = "validation.error.invalid.string.length";
 export const INVALID_IMAGE_SIZE = "validation.error.invalid.image.size";
 export const INVALID_STRING_MIN_MAX = "validation.error.invalid.string.min.max";
-
+export const INVALID_EMAIL = "validation.error.invalid.email";
+export const DEFINE_LEAST_EMAIL= "validation.error.define.least.email";
+export const MULTIPLE_PRIMARY_EMAIL = "validation.error.multiple.primary.email";
+export const NO_PRIMARY_EMAIL = "validation.error.no.primary.email";
 
 //export const IS_NAN = "validation.error.is_nan";
 //export const END_DATE_BEFORE_START_DATE = "validation.error.end_data_before_start_date";
@@ -54,5 +57,21 @@ defineMessages({
     [INVALID_STRING_MIN_MAX]: {
         id: INVALID_STRING_MIN_MAX,
         defaultMessage: "Invalid length: please insert"
+    },
+    [INVALID_EMAIL]: {
+        id: INVALID_EMAIL,
+        defaultMessage: "Invalid email"
+    },
+    [DEFINE_LEAST_EMAIL]: {
+        id: DEFINE_LEAST_EMAIL,
+        defaultMessage: "Define at least one email"
+    },
+    [MULTIPLE_PRIMARY_EMAIL]: {
+        id: MULTIPLE_PRIMARY_EMAIL,
+        defaultMessage: "Multiple primary email"
+    },
+    [NO_PRIMARY_EMAIL]: {
+        id: NO_PRIMARY_EMAIL,
+        defaultMessage: "No primary email"
     }
 });
