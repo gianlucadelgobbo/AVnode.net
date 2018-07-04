2d61bf8b813e5528f1ac08db342f4ce5a1b6e41b
Daniele Salvatore <danieleslvtr@gmail.com>
2018-07-04 21:43:24 +0200

    Add languages

--- app/redux/i18n/locales/index.js
+++ app/redux/i18n/locales/index.js
@@ -2,5 +2,11 @@ import fr from "./fr.json";
 import en from "./en.json";
 import es from "./es.json";
 import it from "./it.json";
+import ru from "./ru.json";
+import pl from "./pl.json";
+import hu from "./hu.json";
+import gr from "./gr.json";
+import de from "./de.json";
+import by from "./by.json";
 
-export default { fr, en, es, it };
+export default {fr, en, es, it, ru, pl, hu, gr, de, by};
