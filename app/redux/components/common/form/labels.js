4237ddd89c6534845f8208d51c3e9067934593da
Pietro Colangelo <pcolangelo80@gmail.com>
2018-07-05 18:48:35 +0200

    Labels Forms i18n

---
+++ app/redux/components/common/form/labels.js
@@ -0,0 +1,18 @@
+import {defineMessages} from 'react-intl';
+
+export const STAGE_NAME = "label.stagename";
+export const PROFILE_URL = "label.profile.url";
+
+/*
+* Define i18n Form Label
+* */
+defineMessages({
+    [STAGE_NAME]: {
+        id: STAGE_NAME,
+        defaultMessage: "Stage Name"
+    },
+    [PROFILE_URL]: {
+        id: PROFILE_URL,
+        defaultMessage: "Profile Url"
+    }
+});
