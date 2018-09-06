c6751312eab4b8ff354d0a8a555d96a6ae7704a7
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-09-06 13:55:35 +0200

    test pages

--- app/routes/pages/show.js
+++ app/routes/pages/show.js
@@ -1,27 +1,34 @@
 const router = require('../router')();
-const User = require('../../models/User');
 const request = require('request');
-//const Crew = require('../../models/Crew');
+
+const logger = require('../../utilities/logger');
 
 router.get('/', (req, res, next) => {
-  res.render('pages/show', {
+/*   res.render('pages/show', {
     title: "body.post_name",
     data: "body"
-  });
-/* request.get({
+  }); */
+  logger.debug("pages");
+  logger.debug(request.get);
+  logger.debug(request);
+  request.get({
       url: 'https://cms.avnode.net/'+global.getLocale()+'/wp-json/wp/v2/mypages'+req.baseUrl,
-      json:true
+      json: true
     }, function (error, response, body) {
+      logger.debug("request");
+      logger.debug(error);
+      logger.debug(response);
+      logger.debug(body);
       if (body) {
         res.render('pages/show', {
           title: body.post_name,
           data: body
         });
       } else {
-
+        res.status(404).render('404', {title:"<span class=\"lnr lnr-warning\" style=\"font-size:  200%;vertical-align:  middle;padding-right: 20px;\"></span><span style=\"vertical-align:  middle;\">"+__("404: Page not found")+"</span>"});
       }
     }
-  ); */
+  );
 });
 
 module.exports = router;
