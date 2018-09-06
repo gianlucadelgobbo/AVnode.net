68e61e36f2ed5365ce7c2a64dbfccd1657d5a9ea
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-09-06 14:03:05 +0200

    Load pages timeout

--- app/routes/pages/show.js
+++ app/routes/pages/show.js
@@ -4,28 +4,17 @@ const request = require('request');
 const logger = require('../../utilities/logger');
 
 router.get('/', (req, res, next) => {
-/*   res.render('pages/show', {
-    title: "body.post_name",
-    data: "body"
-  }); */
-  logger.debug("pages");
-  logger.debug(request.get);
-  logger.debug(request);
   request.get({
       url: 'https://cms.avnode.net/'+global.getLocale()+'/wp-json/wp/v2/mypages'+req.baseUrl,
       json: true
     }, function (error, response, body) {
-      logger.debug("request");
-      logger.debug(error);
-      logger.debug(response);
-      logger.debug(body);
       if (body) {
         res.render('pages/show', {
           title: body.post_name,
           data: body
         });
       } else {
-        res.status(404).render('404', {title:"<span class=\"lnr lnr-warning\" style=\"font-size:  200%;vertical-align:  middle;padding-right: 20px;\"></span><span style=\"vertical-align:  middle;\">"+__("404: Page not found")+"</span>"});
+        res.status(404).render('404', {title:"<span class=\"lnr lnr-warning\" style=\"font-size:  200%;vertical-align:  middle;padding-right: 20px;\"></span><span style=\"vertical-align:  middle;\">"+__("408: Request Timeout")+"</span>"});
       }
     }
   );
