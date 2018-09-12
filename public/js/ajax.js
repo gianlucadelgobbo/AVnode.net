10ee74f7e71780d84fdb0f16b086a76191eabdd0
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-09-13 00:50:12 +0200

    alert

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,3 +1,38 @@
+$(function() {
+  checkCookie()
+});
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var firsttime = getCookie("firsttime");
+  if (!firsttime) {
+    $('#msg_modal').modal({
+      backdrop: true
+    });
+    setCookie("firsttime", true, 10);
+  }
+}
+
 $("#liker").on('click', function(ev) {
   ///likes/?section=performances&id=5a9c32c3606624000000bccb
   ev.preventDefault();
