fd789b500b23dec7d991204a86cc8511487b2689
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-09-14 12:14:37 +0200

    Galleries fix

--- app/models/shared/Media.js
+++ app/models/shared/Media.js
@@ -6,7 +6,7 @@ const adminsez = 'gallery';
 
 const Media = new Schema({
   url: String,
-  slug: { type: String, unique: true },
+  slug: { type: String/* , unique: true */ },
   file: String,
   filename: String,
   preview: String,
