62203e4f922f1b3dbafd93debb4c0a1aa166bbe7
Pietro Colangelo <pcolangelo80@gmail.com>
2018-10-02 11:43:33 +0200

    Adding Validation in AddCrew Form Component

--- app/redux/components/crews/add/validate.js
+++ app/redux/components/crews/add/validate.js
@@ -1,9 +1,14 @@
+import {isValidName, validateLength, isValidSlug} from "../../common/form/validators";
+import {INVALID_STRING_3_50} from "../../common/form/errors";
+
 const validate = values => {
     const errors = {};
 
-    if (!values.name || values.name.trim() === "") {
-        errors.name = 'Name Required';
-    }
+    isValidName({values, name: "stagename", errors});
+
+    isValidSlug({values, name: "slug", errors});
+
+    validateLength({values, name: "slug", min: 3, max: 50, errorKey: INVALID_STRING_3_50, errors});
 
     return errors
 };
