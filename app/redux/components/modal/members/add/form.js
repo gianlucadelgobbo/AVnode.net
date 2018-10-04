3cd2ce832fab4dd64be8dab737bb099b617f0d63
Pietro Colangelo <pcolangelo80@gmail.com>
2018-10-04 11:27:04 +0200

    Adding React Autosuggest in Members Component

--- app/redux/components/modal/members/add/form.js
+++ app/redux/components/modal/members/add/form.js
@@ -3,12 +3,75 @@ import {reduxForm, Field} from "redux-form";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import {FORM_NAME} from './constants';
-import {userAutocompleteSelect} from "../../../common/form/components";
+import {autocompleteComponent} from "../../../common/form/components";
 import validate from './validate';
 import asyncValidate from './asyncValidate';
 import {saveModel} from "../../../crews/members/actions";
 
+const members = [
+    {
+        name: 'John',
+        year: 1972
+    },
+    {
+        name: 'Daniele',
+        year: 2012
+    },
+    {
+        name: 'Marco',
+        year: 1972
+    },
+    {
+        name: 'Luca',
+        year: 2012
+    }
+  ];
+
+const getSuggestions = value => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+  
+    return inputLength === 0 ? [] : members.filter(member =>
+        member.name.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  };
+  
+  const getSuggestionValue = suggestion => suggestion.name;
+  
+  const renderSuggestion = suggestion => (
+    <div>
+      {suggestion.name}
+    </div>
+  )
+
 class AddMembersForm extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+          value: '',
+          suggestions: []
+        };
+      }
+    
+      
+    onChange = (event, { newValue }) => {
+        this.setState({
+        value: newValue
+        });
+    };
+
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+        suggestions: getSuggestions(value)
+        });
+    };
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+          suggestions: []
+        });
+      };
 
     createModelToSave(values){
          //clone obj
@@ -34,6 +97,8 @@ class AddMembersForm extends Component {
        
     }
 
+    
+
     render() {
 
         const {
@@ -41,13 +106,28 @@ class AddMembersForm extends Component {
             handleSubmit
         } = this.props;
 
+        const { value, suggestions } = this.state;
+
+        const inputProps = {
+            placeholder: 'Type a members',
+            value,
+            onChange: this.onChange
+          };
+      
+
         return (
             <form onSubmit={handleSubmit(this.submitForm.bind(this))}>
 
                 <Field
                     name="members"
-                    component={userAutocompleteSelect}
+                    component={autocompleteComponent}
+                    inputProps={inputProps}
+                    suggestions ={ suggestions }
                     placeholder="Members"
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
                 />
 
                 <hr/>
