6eb973fe74ebbbeb4c07282a4178ce41f520d77c
Pietro Colangelo <pcolangelo80@gmail.com>
2018-10-11 12:19:00 +0200

    Adding FetchSlug to AsyncValidate

--- app/redux/components/crews/add/asyncValidate.js
+++ app/redux/components/crews/add/asyncValidate.js
@@ -1,12 +1,13 @@
 import {validateSlug, validateAddress} from "../../common/form/validators";
-import {checkIfError} from '../../common/form'
+import {checkIfError} from '../../common/form';
+import {fetchSlugCrew as fetchSlug} from '../../../api';
 
 const asyncValidate = (values, dispatch, state) => {
     const promises = [];
     const result = {};
 
      // slug
-    validateSlug({value: values.slug, previousValue: state.initialValues.slug, promises, result});
+    validateSlug({value: values.slug, previousValue: state.initialValues.slug, promises, result, fetchSlug});
 
      return Promise.all(promises)
         .then(() => {
