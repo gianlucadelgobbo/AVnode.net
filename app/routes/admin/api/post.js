919e03411cd0c79b66d3139c88be709890d8985c
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-10-27 12:37:25 +0200

    Crew new save

--- app/routes/admin/api/post.js
+++ app/routes/admin/api/post.js
@@ -19,6 +19,7 @@ router.postData = (req, res) => {
   logger.debug(req.body);
   if (config.cpanel[req.params.sez] && config.cpanel[req.params.sez].forms.new) {
     let select = config.cpanel[req.params.sez].forms.new.select;
+    let selectaddon = config.cpanel[req.params.sez].forms.new.selectaddon;
     let post = {};
     //for (const item in select) if(req.body[item]) post[item] = req.body[item];
     // db.users.update({slug:'gianlucadelgobbo'},{$unset: {oldpassword:""}});
@@ -27,8 +28,13 @@ router.postData = (req, res) => {
     for (const item in select) if(req.body[item]) {
       post[item] = req.body[item];
     }
+    for (const item in selectaddon) {
+      post[item] = selectaddon[item];
+    }
     logger.debug('postpostpostpostpostpost');
     logger.debug(post);
+    logger.debug('session');
+    logger.debug(req.sessions);
 
     Models[config.cpanel[req.params.sez].model]
     .create(post, (err, data) => {
@@ -37,10 +43,55 @@ router.postData = (req, res) => {
         console.log(err);
         res.status(400).json(err);
       } else {
-        logger.debug('DataDataDataDataDataData');
+        const id = req.user.id;
+        logger.debug('req.user.id');
+        logger.debug(req.user.id);
+        Models[config.cpanel[req.params.sez].model]
+        .findById(id, req.params.sez, (err, user) => {
+          logger.debug('useruseruseruseruseruseruseruseruseruseruseruser');
+          logger.debug(user);
+          if (!err) {
+            if (user) {
+              user[req.params.sez].push(data._id);
+              logger.debug('useruseruseruseruseruseruseruseruseruseruseruser');
+              logger.debug(user);
+              user.save((err) => {
+                if (err) {
+                  console.log('err');
+                  console.log(err);
+                  res.status(400).json(err);
+                } else {
+                  select = req.query.pure ? config.cpanel[req.params.sez].list.select : Object.assign(config.cpanel[req.params.sez].list.select, config.cpanel[req.params.sez].list.selectaddon);
+                  const populate = req.query.pure ? [] : config.cpanel[req.params.sez].list.populate;
+                    
+                  Models[config.cpanel[req.params.sez].list.model]
+                  .findById(id)
+                  .select(select)
+                  .populate(populate)
+                  .exec((err, data) => {
+                    if (err) {
+                      res.status(500).json({ error: `${JSON.stringify(err)}` });
+                    } else {
+                      let send = {_id: data._id};
+                      for (const item in config.cpanel[req.params.sez].list.select) send[item] = data[item];
+                      console.log('sendsendsendsendsendsendsend');
+                      console.log(send);
+                      res.json(send);
+                    }
+                  });
+                }
+              });
+            } else {
+              res.status(204).json({ error: `DOC_NOT_FOUND` });
+            }
+          } else {
+            res.status(500).json({ error: `${JSON.stringify(err)}` });
+          }
+        });
+/*         logger.debug('DataDataDataDataDataData');
         logger.debug(data);
         res.json(data);
-      }
+ */      }
     });
   } else {
     res.status(404).json({ error: `API_NOT_FOUND` });
