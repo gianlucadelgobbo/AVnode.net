3433021aa04a4e858a39d38335a6fec6540406df
piero80 <pcolangelo80@gmail.com>
2018-10-28 15:35:33 +0100

    Implementation Add New Crew

--- app/redux/api.js
+++ app/redux/api.js
@@ -465,7 +465,11 @@ export const removeCrew = ({ id }) => {
   });
 };
 
-export const postCrew = obj => axios.post(`crews/new/`, obj);
+export const postCrew = obj => {
+  return axios.post(`crews/new/`, obj).then(result => {
+    return result.data;
+  });
+};
 
 // - public
 
