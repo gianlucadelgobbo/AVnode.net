ecd09b202b4c3e0283156481851b6fc47b3bd992
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-10-31 20:01:19 +0100

    Participate save

---
+++ app/models/Subscription.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const subscriptionSchema = new Schema({
+  event: { type: Schema.ObjectId, ref: 'Event', required: true },
+  call: { type: Number, required: true},
+  topics: { type: [String], minlength: 1},
+  performance: { type: Schema.ObjectId, ref: 'Performance', required: true },
+  reference: { type: Schema.ObjectId, ref: 'User', required: true },
+  subscriptions: {
+    type: [{
+      subscriber_id: { type: Schema.ObjectId, ref: 'User' },
+      days: { type: [Date], minlength: 1},
+      packages: [{
+        name: String,
+        price: Number,
+        description: String,
+        personal: { type: Boolean, default: false },
+        requested: { type: Boolean, default: false },
+        allow_multiple: { type: Boolean, default: false },
+        allow_options: { type: Boolean, default: false },
+        options_name: String,
+        options: String,
+        option: String,
+        daily: { type: Boolean, default: false },
+        start_date: Date,
+        end_date: Date
+      }]
+    }],
+    minlength: 1
+  }
+},
+{
+  timestamps: true,
+  collection: 'subscriptions',
+  toObject: {
+    virtuals: true
+  },
+  toJSON: {
+    virtuals: true
+  }
+});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+module.exports = Subscription;
