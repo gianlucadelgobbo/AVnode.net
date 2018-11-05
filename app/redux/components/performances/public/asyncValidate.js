42a33b0b50d11791a21c5028844013219250e4ca
piero80 <pcolangelo80@gmail.com>
2018-11-05 20:15:03 +0100

    Adding Slug in Public and in Add Performance

--- app/redux/components/performances/public/asyncValidate.js
+++ app/redux/components/performances/public/asyncValidate.js
@@ -1,10 +1,30 @@
+import {
+  validateSlugWithID,
+  validateAddress
+} from "../../common/form/validators";
+import { checkIfError } from "../../common/form";
+
 const asyncValidate = (values, dispatch, state) => {
+  const promises = [];
+  const result = {};
 
-    const promises = [];
-    const result = {};
+  // slug
+  validateSlugWithID({
+    value: values.slug,
+    previousValue: state.initialValues.slug,
+    promises,
+    result,
+    id: state.model.id,
+    section: "performances"
+  });
 
-    return Promise.all(promises).then(() => result);
-
+  return Promise.all(promises)
+    .then(() => {
+      return checkIfError(result);
+    })
+    .catch(() => {
+      return checkIfError(result);
+    });
 };
 
-export default asyncValidate
+export default asyncValidate;
