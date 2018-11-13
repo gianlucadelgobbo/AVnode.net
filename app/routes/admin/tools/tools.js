db13af9d60e87a8d8f70f7e1734f4b0fc0dead9b
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-11-13 23:29:49 +0100

    Images fix

--- app/routes/admin/tools/tools.js
+++ app/routes/admin/tools/tools.js
@@ -476,6 +476,7 @@ router.get('/files/performanceimages', (req, res) => {
         const file = performances[performance].image.file;
         const fileName = file.substring(file.lastIndexOf('/') + 1); // file.jpg this.file.file.substr(19)
         const fileFolder = file.substring(0, file.lastIndexOf('/')); // /warehouse/2017/03
+        const oldPath = fileFolder.replace("/glacier/performances_originals/", "/warehouse/"); // /warehouse/2017/03
         const publicPath = fileFolder.replace("/glacier/performances_originals/", "/warehouse/performances/"); // /warehouse/2017/03
         const fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
         const fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
@@ -486,7 +487,10 @@ router.get('/files/performanceimages', (req, res) => {
         for(let format in config.cpanel[adminsez].forms.image.components.image.config.sizes) {
           performances[performance].image.imageFormatsExists[format] = fs.existsSync(global.appRoot+performances[performance].image.imageFormats[format]);
         }
-      //}
+        if (!performances[performance].image.exists) {
+          performances[performance].image.mkdir = `mkdir ${fileFolder.replace("/glacier/", "glacier/")}`;
+          performances[performance].image.find = `find ${oldPath.replace("/warehouse/", "/space/PhpMysql2015/sites/flxer/warehouse/")} -name '${fileName}' -exec cp "{}" ${fileFolder.replace("/glacier/", "glacier/")} \\;`;
+        }
       data.push(performances[performance].image);
     }
     console.log(req.path);
