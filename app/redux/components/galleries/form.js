b637f986fca60063591cf5b9cbf763d4816e7eaf
piero80 <pcolangelo80@gmail.com>
2018-11-26 20:33:32 +0100

    Change Performances Galleries

--- app/redux/components/galleries/form.js
+++ app/redux/components/galleries/form.js
@@ -1,51 +1,47 @@
-import React, { Component } from 'react';
-import {reduxForm, Field} from "redux-form";
-import {FORM_NAME} from './constants'
-import {sort} from "../common/form/components";
-import validate from './validate'
-import asyncValidate from './asyncValidate'
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { FORM_NAME } from "./constants";
+import { sort } from "../common/form/components";
+import validate from "./validate";
+import asyncValidate from "./asyncValidate";
 
 class GalleriesForm extends Component {
+  render() {
+    const {
+      submitting,
+      handleSubmit,
+      showModal,
+      onSubmit,
+      onRemove
+    } = this.props;
 
-    render() {
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Field
+          name="galleries"
+          component={sort}
+          showModal={showModal}
+          onRemove={onRemove}
+        />
 
-        const {
-            submitting,
-            handleSubmit,
-            showModal,
-            onSubmit,
-            onRemove
-        } = this.props;
+        <hr />
 
-        return (
-            <form onSubmit={handleSubmit(onSubmit)}>
-
-                <Field
-                    name="galleries"
-                    component={sort}
-                    showModal={showModal}
-                    onRemove={onRemove}
-                />
-
-                <hr/>
-
-                <button
-                    type="submit"
-                    disabled={submitting}
-                    className="btn btn-primary btn-lg btn-block">
-                    {submitting ? "Saving..." : "Save"}
-                </button>
-
-            </form>
-        );
-    }
-
+        <button
+          type="submit"
+          disabled={submitting}
+          className="btn btn-primary btn-lg btn-block"
+        >
+          {submitting ? "Saving..." : "Save"}
+        </button>
+      </form>
+    );
+  }
 }
 
 export default reduxForm({
-    form: FORM_NAME,
-    enableReinitialize: true,
-    keepDirtyOnReinitialize: true,
-    validate,
-    asyncValidate,
+  form: FORM_NAME,
+  enableReinitialize: true,
+  keepDirtyOnReinitialize: true,
+  validate,
+  asyncValidate
 })(GalleriesForm);
