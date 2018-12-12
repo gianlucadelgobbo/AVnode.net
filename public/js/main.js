ae70c8dc00fe46589b07654779fab0dc51bce082
donostiastudio <donostiasocialclub@gmail.com>
2018-12-12 18:42:55 +0100

    bootstrap4

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
  
     $( ".event_main_image_wrapper" ).click(function() {
-      $( ".event_main_image_wrapper" ).toggleClass("expanded");
+    //   $( ".event_main_image_wrapper" ).toggleClass("expanded");
+    $( ".event_main_image_wrapper" ).addClass("expanded");    
     });
 
     
