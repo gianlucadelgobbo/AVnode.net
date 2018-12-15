9e8b6d7d8367d863deecf26c3cc911143e6125fa
piero80 <pcolangelo80@gmail.com>
2018-12-15 15:51:22 +0100

    Change Add Performance

--- app/redux/components/performances/index.js
+++ app/redux/components/performances/index.js
@@ -1,58 +1,66 @@
-import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
-import ModelTable from './table'
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
-import {MODAL_ADD_PERFORMANCE} from "../modal/constants";
-import {showModal} from "../modal/actions";
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import ModelTable from "./table";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { MODAL_ADD_PERFORMANCE } from "../modal/constants";
+import { showModal } from "../modal/actions";
+import { BrowserRouter as Router } from "react-router-dom";
 
 class Performances extends Component {
+  render() {
+    const { showModal, location, history } = this.props;
 
-    render() {
-
-        const {showModal} = this.props;
-
-        return (
-            <div className="row">
-                <div className="col-md-12">
-
-                    <div className="row">
-                        <div className="col-md-12">
-                            <Button
-                                bsStyle="success"
-                                className="pull-right"
-                                onClick={() => showModal({
-                                    type: MODAL_ADD_PERFORMANCE
-                                })}>
-                                <i className="fa fa-plus" data-toggle="tooltip" data-placement="top"/>
-                            </Button>
-
-                        </div>
-                    </div>
-
-                    <hr/>
-
-                    <div className="row">
-                        <div className="col-md-12">
-                            <ModelTable/>
-                        </div>
-                    </div>
-
-                </div>
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            <div className="col-md-12">
+              <Button
+                bsStyle="success"
+                className="pull-right"
+                onClick={() =>
+                  showModal({
+                    type: MODAL_ADD_PERFORMANCE,
+                    props: { history }
+                  })
+                }
+              >
+                <i
+                  className="fa fa-plus"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                />
+              </Button>
             </div>
-        );
-    }
+          </div>
+
+          <hr />
+
+          <div className="row">
+            <div className="col-md-12">
+              <ModelTable />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = () => ({});
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    showModal
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      showModal
+    },
+    dispatch
+  );
 
 Performances = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Performances);
 
 export default Performances;
