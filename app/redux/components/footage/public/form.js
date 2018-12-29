085054a3bcc1bc2b9e1504dfce07145753b7c722
piero80 <pcolangelo80@gmail.com>
2018-12-29 18:01:46 +0100

    Upload Button Conditional

--- app/redux/components/footage/public/form.js
+++ app/redux/components/footage/public/form.js
@@ -1,123 +1,126 @@
-import React, { Component } from 'react';
-import {reduxForm, Field, FieldArray} from "redux-form";
-import {FORM_NAME} from './constants';
+import React, { Component } from "react";
+import { reduxForm, Field, FieldArray } from "redux-form";
+import { FORM_NAME } from "./constants";
 import {
-    renderDropzoneInput,
-    inputText,
-    textareaMultiTab, tagsInput, fieldWithLabel
+  renderDropzoneInput,
+  inputText,
+  textareaMultiTab,
+  tagsInput,
+  fieldWithLabel
 } from "../../common/form/components";
-import validate from './validate';
-import {injectIntl} from 'react-intl';
-import {ABOUT, FOOTAGE_NAME, FOOTAGE_URL, AUTHORS, TAGS} from "../../common/form/labels";
+import validate from "./validate";
+import { injectIntl } from "react-intl";
+import {
+  ABOUT,
+  FOOTAGE_NAME,
+  FOOTAGE_URL,
+  AUTHORS,
+  TAGS
+} from "../../common/form/labels";
 
 class FootagePublicForm extends Component {
+  getIntlString = obj => {
+    const { intl } = this.props;
+    return intl.formatMessage(obj);
+  };
 
-    getIntlString = (obj) => {
-        const {intl} = this.props;
-        return intl.formatMessage(obj)
-    };
+  render() {
+    const {
+      submitting,
+      handleSubmit,
+      tabs,
+      labels,
+      showModal,
+      onSubmit,
+      delimiters,
+      handleDelete,
+      handleTagClick,
+      handleAddition,
+      tags,
+      uploadFile
+    } = this.props;
 
-    render() {
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Field
+          name="title"
+          component={inputText}
+          placeholder={this.getIntlString({ id: FOOTAGE_NAME })}
+        />
 
-        const {
-            submitting,
-            handleSubmit,
-            tabs,
-            labels,
-            showModal,
-            onSubmit,
-            delimiters,
-            handleDelete,
-            handleTagClick,
-            handleAddition,
-            tags,
-            uploadFile
-        } = this.props;
+        <Field
+          name="slug"
+          component={inputText}
+          placeholder={this.getIntlString({ id: FOOTAGE_URL })}
+        />
 
-        return (
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <Field
+          name="media"
+          component={renderDropzoneInput}
+          showModal={showModal}
+          //properties={["jpeg, png"]}
+          accept="video/mp4, video/quicktime, video/x-flv, video/x-ms-wmv, video/x-msvideo"
+          maxSize={21474836480}
+          uploadFile={uploadFile}
+          uploadButton={true}
+          //accept={this.renderImageType()}
+        />
 
+        <FieldArray
+          name="abouts"
+          component={textareaMultiTab}
+          tabs={tabs}
+          labels={labels}
+          placeholder={this.getIntlString({ id: ABOUT })}
+        />
 
-                <Field
-                    name="title"
-                    component={inputText}
-                    placeholder={this.getIntlString({id:FOOTAGE_NAME})}
-                />
+        <br />
 
-                <Field
-                    name="slug"
-                    component={inputText}
-                    placeholder={this.getIntlString({id:FOOTAGE_URL})}
-                />
+        <FieldArray
+          name="users"
+          component={fieldWithLabel}
+          placeholder={this.getIntlString({ id: AUTHORS })}
+          showModal={showModal}
+        />
 
-                <Field
-                    name="media"
-                    component={renderDropzoneInput}
-                    showModal={showModal}
-                    //properties={["jpeg, png"]}
-                    accept="video/mp4, video/quicktime, video/x-flv, video/x-ms-wmv, video/x-msvideo"
-                    maxSize={21474836480}
-                    uploadFile={uploadFile}
-                    //accept={this.renderImageType()}
-                />
+        <br />
 
-                <FieldArray
-                    name="abouts"
-                    component={textareaMultiTab}
-                    tabs={tabs}
-                    labels={labels}
-                    placeholder={this.getIntlString({id:ABOUT})}
-                />
+        <Field
+          name="tags"
+          component={tagsInput}
+          tags={tags}
+          delimiters={delimiters}
+          handleDelete={handleDelete}
+          handleTagClick={handleTagClick}
+          handleAddition={handleAddition}
+          placeholder={this.getIntlString({ id: TAGS })}
+        />
 
-                <br/>
+        <br />
 
-                <FieldArray
-                    name="users"
-                    component={fieldWithLabel}
-                    placeholder={this.getIntlString({id:AUTHORS})}
-                    showModal={showModal}
-                />
+        <hr />
 
-                <br/>
-
-                <Field
-                    name="tags"
-                    component={tagsInput}
-                    tags={tags}
-                    delimiters={delimiters}
-                    handleDelete={handleDelete}
-                    handleTagClick={handleTagClick}
-                    handleAddition={handleAddition}
-                    placeholder={this.getIntlString({id:TAGS})}
-                />
-
-                <br/>
-
-                <hr/>
-
-                <button
-                    type="submit"
-                    disabled={submitting}
-                    className="btn btn-primary btn-lg btn-block">
-                    {submitting ? "Saving..." : "Save"}
-                </button>
-
-            </form>
-        );
-    }
-
+        <button
+          type="submit"
+          disabled={submitting}
+          className="btn btn-primary btn-lg btn-block"
+        >
+          {submitting ? "Saving..." : "Save"}
+        </button>
+      </form>
+    );
+  }
 }
 
 FootagePublicForm = reduxForm({
-    form: FORM_NAME,
-    enableReinitialize: true,
-    keepDirtyOnReinitialize: true,
-    validate,
-    //asyncValidate,
-    //asyncBlurFields: ['slug', 'addresses[]']
+  form: FORM_NAME,
+  enableReinitialize: true,
+  keepDirtyOnReinitialize: true,
+  validate
+  //asyncValidate,
+  //asyncBlurFields: ['slug', 'addresses[]']
 })(FootagePublicForm);
 
 FootagePublicForm = injectIntl(FootagePublicForm);
 
 export default FootagePublicForm;
-
