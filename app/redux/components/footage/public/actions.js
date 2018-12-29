91b703593dfbeb4df1d0f8df5292e98d4c94b585
piero80 <pcolangelo80@gmail.com>
2018-12-29 19:47:53 +0100

    manage error

--- app/redux/components/footage/public/actions.js
+++ app/redux/components/footage/public/actions.js
@@ -1,29 +1,36 @@
-import * as api from '../../../api';
-import {footage} from '../schema'
-import {fetchModel as generateFetchModel, saveModel as generateSaveModel, removeModel as generateRemoveModel} from '../../../actions'
+import * as api from "../../../api";
+import { footage } from "../schema";
+import {
+  fetchModel as generateFetchModel,
+  saveModel as generateSaveModel,
+  removeModel as generateRemoveModel
+} from "../../../actions";
 import * as selectors from "../selectors";
-import * as constants from '../constants'
+import * as constants from "../constants";
 
-export const fetchModel = ({id} = {}) => generateFetchModel({
+export const fetchModel = ({ id } = {}) =>
+  generateFetchModel({
     selectors,
     constants,
     request: api.fetchFootagePublic,
     schema: footage,
     id
-});
+  });
 
-export const saveModel = (model) => generateSaveModel({
+export const saveModel = model =>
+  generateSaveModel({
     selectors,
     constants,
     request: api.saveFootagePublic,
     schema: footage,
-    model,
-});
+    model
+  });
 
-export const removeModel = (model) => generateRemoveModel({
+export const removeModel = model =>
+  generateRemoveModel({
     selectors,
     constants,
     request: api.removeFootagePublic,
     schema: footage,
-    model,
-});
+    model
+  });
