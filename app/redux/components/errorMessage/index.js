b2a328b5d354e27d63928407045158b762f7f9f1
Daniele Salvatore <danieleslvtr@gmail.com>
2018-12-30 16:50:19 +0100

    Make <ErrorMessage /> compatible with multiple errors

--- app/redux/components/errorMessage/index.js
+++ app/redux/components/errorMessage/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {injectIntl} from "react-intl";
 import Parser from 'html-react-parser';
 
@@ -9,16 +9,50 @@ class ErrorMessage extends Component {
         return intl.formatMessage(obj)
     };
 
+    printError(message) {
+        return <div className="alert alert-danger" role="alert">
+            Ops... {Parser(message)}
+        </div>
+    }
+
+    getNormalizedErrors(errorMessage) {
+
+        const keys = Object.keys(errorMessage);
+        let unionOfAllTheErrors = [];
+
+        keys.forEach(k => {
+            const fieldErrors = errorMessage[k];
+            fieldErrors.forEach(f => {
+                unionOfAllTheErrors = unionOfAllTheErrors.concat(f.err);
+            })
+        });
+
+        return unionOfAllTheErrors;
+    }
+
     render() {
 
         const {errorMessage} = this.props;
+        const isMultipleError = Array.isArray(errorMessage);
 
         return (
             <div className="row">
                 <div className="col-md-12">
-                    <div className="alert alert-danger" role="alert">
-                        Ops... {Parser(errorMessage)}
-                    </div>
+
+                    {isMultipleError ?
+                        this.printError(errorMessage)
+                        :
+                        <ul className="list-unstyled">
+
+                            {this.getNormalizedErrors(errorMessage)
+                                .map((e, index) => <li key={index}>
+                                        {this.printError(e)}
+                                    </li>
+                                )}
+
+                        </ul>
+                    }
+
                 </div>
             </div>
         );
