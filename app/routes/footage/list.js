1ca9176d40a84234894784518923f1c85e6c1ee6
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-01-02 21:37:56 +0100

    encoding

--- app/routes/footage/list.js
+++ app/routes/footage/list.js
@@ -14,7 +14,7 @@ router.get('/:filter/:sorting/:page', (req, res) => {
 router.get('/:filter/:sorting', (req, res) => {
   if (req.params.sorting == "tobeencoded") {
     Model
-    .find({"media.encoded":{$exists:true},"media.encoded": {$ne:true},"media.encoded": {$ne:1}})
+    .findOne({"media.encoded":{$exists:true},"media.encoded": {$ne:true},"media.encoded": {$ne:1}})
     .select({media:1})
     .exec((err, data) => {
       if (err) {
