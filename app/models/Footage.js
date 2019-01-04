f2bae5b6e66bdd6c10252a17a78c65b64812a43f
piero80 <pcolangelo80@gmail.com>
2019-01-04 19:15:23 +0100

    Add Footage

--- app/models/Footage.js
+++ app/models/Footage.js
@@ -1,84 +1,116 @@
-const config = require('getconfig');
-const mongoose = require('mongoose');
+const config = require("getconfig");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const indexPlugin = require('../utilities/elasticsearch/Footage');
-const helper = require('../utilities/helper');
+const indexPlugin = require("../utilities/elasticsearch/Footage");
+const helper = require("../utilities/helper");
 
-const About = require('./shared/About');
-const Media = require('./shared/Media');
+const About = require("./shared/About");
+const Media = require("./shared/Media");
 
-const adminsez = 'footage';
+const adminsez = "footage";
 
-const footageSchema = new Schema({
-  old_id : String,
-  creation_date: Date,
-  slug: { type: String, unique: true },
-  title: String,
-  is_public: { type: Boolean, default: false },
-  media: Media,
-  abouts: [About],
-  stats: {},
-  users: [{ type : Schema.ObjectId, ref : 'UserShow' }],
-  playlists: [{ type : Schema.ObjectId, ref : 'Playlist' }],
-  tags: [{
-    old_id : String,
-    tag : String,
-    tot : Number,
-    required : Boolean,
-    exclusive : Boolean
-  }]
-}, {
-  collection: 'footage',
-  timestamps: true,
-  toObject: {
-    virtuals: true
+const footageSchema = new Schema(
+  {
+    old_id: String,
+    creation_date: Date,
+    slug: { type: String, unique: true },
+    title: String,
+    is_public: { type: Boolean, default: false },
+    media: Media,
+    abouts: [About],
+    stats: {},
+    users: [{ type: Schema.ObjectId, ref: "UserShow" }],
+    playlists: [{ type: Schema.ObjectId, ref: "Playlist" }],
+    tags: [
+      {
+        old_id: String,
+        tag: String,
+        tot: Number,
+        required: Boolean,
+        exclusive: Boolean
+      }
+    ]
   },
-  toJSON: {
-    virtuals: true
+  {
+    collection: "footage",
+    timestamps: true,
+    toObject: {
+      virtuals: true
+    },
+    toJSON: {
+      virtuals: true
+    }
   }
-});
+);
 
 // Return thumbnail
-footageSchema.virtual('imageFormats').get(function () {
+footageSchema.virtual("imageFormats").get(function() {
   let imageFormats = {};
   if (this.media.encoded) {
-    for(let format in config.cpanel[adminsez].forms.public.components.media.config.sizes) {
-      imageFormats[format] = config.cpanel[adminsez].forms.public.components.media.config.sizes[format].default;
-    }  
+    for (let format in config.cpanel[adminsez].forms.public.components.media
+      .config.sizes) {
+      imageFormats[format] =
+        config.cpanel[adminsez].forms.public.components.media.config.sizes[
+          format
+        ].default;
+    }
   } else {
-    for(let format in config.cpanel[adminsez].forms.public.components.media.config.sizes) {
-      imageFormats[format] = config.cpanel[adminsez].forms.public.components.media.config.sizes[format].tobeencoded;
-      console.log(config.cpanel[adminsez].forms.public.components.media.config.sizes[format]);
-    }  
+    for (let format in config.cpanel[adminsez].forms.public.components.media
+      .config.sizes) {
+      imageFormats[format] =
+        config.cpanel[adminsez].forms.public.components.media.config.sizes[
+          format
+        ].tobeencoded;
+      console.log(
+        config.cpanel[adminsez].forms.public.components.media.config.sizes[
+          format
+        ]
+      );
+    }
   }
   if (this.media && this.media.preview) {
     const serverPath = this.media.preview;
-    const localFileName = serverPath.substring(serverPath.lastIndexOf('/') + 1); // file.jpg this.file.file.substr(19)
-    const localPath = serverPath.substring(0, serverPath.lastIndexOf('/')).replace('/glacier/footage_previews/', process.env.WAREHOUSE+'/warehouse/footage_previews/'); // /warehouse/2017/03
-    const localFileNameWithoutExtension = localFileName.substring(0, localFileName.lastIndexOf('.'));
-    const localFileNameExtension = localFileName.substring(localFileName.lastIndexOf('.') + 1);
+    const localFileName = serverPath.substring(serverPath.lastIndexOf("/") + 1); // file.jpg this.file.file.substr(19)
+    const localPath = serverPath
+      .substring(0, serverPath.lastIndexOf("/"))
+      .replace(
+        "/glacier/footage_previews/",
+        process.env.WAREHOUSE + "/warehouse/footage_previews/"
+      ); // /warehouse/2017/03
+    const localFileNameWithoutExtension = localFileName.substring(
+      0,
+      localFileName.lastIndexOf(".")
+    );
+    const localFileNameExtension = localFileName.substring(
+      localFileName.lastIndexOf(".") + 1
+    );
     // console.log('localFileName:' + localFileName + ' localPath:' + localPath + ' localFileNameWithoutExtension:' + localFileNameWithoutExtension);
-    for(let format in config.cpanel[adminsez].forms.public.components.media.config.sizes) {
-      imageFormats[format] = `${localPath}/${config.cpanel[adminsez].forms.public.components.media.config.sizes[format].folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
+    for (let format in config.cpanel[adminsez].forms.public.components.media
+      .config.sizes) {
+      imageFormats[format] = `${localPath}/${
+        config.cpanel[adminsez].forms.public.components.media.config.sizes[
+          format
+        ].folder
+      }/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
     }
   }
   return imageFormats;
 });
 
-footageSchema.virtual('about').get(function (req) {
-  let about = __('Text is missing');
+footageSchema.virtual("about").get(function(req) {
+  let about = __("Text is missing");
   let aboutA = [];
   if (this.abouts && this.abouts.length) {
     aboutA = this.abouts.filter(item => item.lang === global.getLocale());
     if (aboutA.length && aboutA[0].abouttext) {
-      about = aboutA[0].abouttext.replace(/\r\n/g, '<br />');
+      about = aboutA[0].abouttext.replace(/\r\n/g, "<br />");
     } else {
       aboutA = this.abouts.filter(item => item.lang === config.defaultLocale);
       if (aboutA.length && aboutA[0].abouttext) {
-        about = aboutA[0].abouttext.replace(/\r\n/g, '<br />');
+        about = aboutA[0].abouttext.replace(/\r\n/g, "<br />");
       }
     }
-    about = about.replace(new RegExp(/\n/gi)," <br />");
+    about = about.replace(new RegExp(/\n/gi), " <br />");
 
     about = helper.linkify(about);
 
@@ -86,26 +118,31 @@ footageSchema.virtual('about').get(function (req) {
   }
 });
 
-footageSchema.virtual('description').get(function (req) {
-  let about = __('Text is missing');
+footageSchema.virtual("description").get(function(req) {
+  let about = __("Text is missing");
   let aboutA = [];
   if (this.abouts && this.abouts.length) {
     aboutA = this.abouts.filter(item => item.lang === global.getLocale());
     if (aboutA.length && aboutA[0].abouttext) {
-      about = aboutA[0].abouttext.replace(/\r\n/g, '<br />');
+      about = aboutA[0].abouttext.replace(/\r\n/g, "<br />");
     } else {
       aboutA = this.abouts.filter(item => item.lang === config.defaultLocale);
       if (aboutA.length && aboutA[0].abouttext) {
-        about = aboutA[0].abouttext.replace(/\r\n/g, '<br />');
+        about = aboutA[0].abouttext.replace(/\r\n/g, "<br />");
       }
     }
-    about = about.replace(new RegExp(/<(?:.|\n)*?>/gm), "").trim().replace(/  /g , " ");
+    about = about
+      .replace(new RegExp(/<(?:.|\n)*?>/gm), "")
+      .trim()
+      .replace(/  /g, " ");
 
     descriptionA = about.split(" ");
     let descriptionShort = "";
-    for(let item in descriptionA) if ((descriptionShort+" "+descriptionA[item]).trim().length<300) descriptionShort+=descriptionA[item]+" ";
+    for (let item in descriptionA)
+      if ((descriptionShort + " " + descriptionA[item]).trim().length < 300)
+        descriptionShort += descriptionA[item] + " ";
     descriptionShort = descriptionShort.trim();
-    if (descriptionShort.length < about.length) descriptionShort+"..."
+    if (descriptionShort.length < about.length) descriptionShort + "...";
     return descriptionShort;
   }
 });
@@ -144,16 +181,10 @@ footageSchema.virtual('publicUrl').get(function () {
 });
 */
 
-footageSchema.pre('remove', function(next) {
+footageSchema.pre("remove", function(next) {
   const footage = this;
-  footage.model('User').updateMany(
-    { $pull: { footage: footage._id } },
-    next
-  );
-  footage.model('Crew').updateMany(
-    { $pull: { footage: footage._id } },
-    next
-  );
+  footage.model("User").updateMany({ $pull: { footage: footage._id } }, next);
+  footage.model("Crew").updateMany({ $pull: { footage: footage._id } }, next);
 });
 
 /*
@@ -216,6 +247,6 @@ footageSchema.virtual('imageUrl').get(function () {
 
 footageSchema.plugin(indexPlugin());
 
-const Footage = mongoose.model('Footage', footageSchema);
+const Footage = mongoose.model("Footage", footageSchema);
 
 module.exports = Footage;
