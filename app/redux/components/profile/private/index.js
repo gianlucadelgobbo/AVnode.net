35a9978e1a82340e5cd1d93bba82949be3b413ae
piero80 <pcolangelo80@gmail.com>
2019-01-13 16:36:50 +0100

    Bug Fixed Date in Private and Saved in Profile

--- app/redux/components/profile/private/index.js
+++ app/redux/components/profile/private/index.js
@@ -1,218 +1,236 @@
-import React, { Component } from 'react';
-import {FormattedMessage} from 'react-intl';
-import {bindActionCreators} from "redux";
-import LateralMenu from '../lateralMenu';
-import Form from './form';
-import {connect} from 'react-redux';
-import {fetchModel, saveModel} from "./actions";
-import {showModal} from "../../modal/actions";
-import Loading from '../../loading';
-import ErrorMessage from '../../errorMessage';
-import ItemNotFound from '../../itemNotFound';
-import {fetchList as fetchCountries} from '../../countries/actions';
-import {getList as getCountries} from '../../countries/selectors';
-import {MODAL_SAVED} from "../../modal/constants";
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { bindActionCreators } from "redux";
+import LateralMenu from "../lateralMenu";
+import Form from "./form";
+import { connect } from "react-redux";
+import { fetchModel, saveModel } from "./actions";
+import { showModal } from "../../modal/actions";
+import Loading from "../../loading";
+import ErrorMessage from "../../errorMessage";
+import ItemNotFound from "../../itemNotFound";
+import { fetchList as fetchCountries } from "../../countries/actions";
+import { getList as getCountries } from "../../countries/selectors";
+import { MODAL_SAVED } from "../../modal/constants";
 import {
-    getDefaultModel,
-    getDefaultModelErrorMessage,
-    getDefaultModelIsFetching,
-    getErrorMessage,
-    getIsFetching
+  getDefaultModel,
+  getDefaultModelErrorMessage,
+  getDefaultModelIsFetching
 } from "../selectors";
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
-import moment from 'moment';
-import axios from 'axios';
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+import moment from "moment";
+import axios from "axios";
+import { DATE_FORMAT } from "../../../conf";
 
 /*
-* Responsabilita'
-* - Get form's initial values from redux state here
-* - pass initial values to form
-* - dispatch the action to save the model
-* */
+ * Responsabilita'
+ * - Get form's initial values from redux state here
+ * - pass initial values to form
+ * - dispatch the action to save the model
+ * */
 
 class ProfilePrivate extends Component {
+  componentDidMount() {
+    const { fetchModel, fetchCountries } = this.props;
+    fetchModel();
+    fetchCountries();
+  }
 
+  // Convert form values to API model
+  createModelToSave(values) {
+    //clone obj
+    let model = Object.assign({}, values);
 
-    componentDidMount() {
-        const {fetchModel, fetchCountries} = this.props;
-        fetchModel();
-        fetchCountries();
+    //convert Gender
+    model.gender = values.gender.value;
+    //convert Lang
+    model.lang = values.lang.value;
+    //convert Birthday
+    model.birthday = moment(values.birthday)
+      .utc()
+      .format();
+    // Convert citizenship
+    model.citizenship = model.citizenship.filter(a => a).map(a => a.value);
+    // Convert addresses_private
+    model.addresses_private = model.addresses_private.map(a => {
+      const originalString = a.text;
+      const split = originalString.split(",");
+      const country = split[split.length - 1].trim();
+      const street = split[0].trim();
+      const locality = split[1].trim();
+      const formatted_address = originalString;
+      const geometry = a.geometry;
+      return { formatted_address, street, locality, country, geometry };
+    });
+    // Convert Phone Number
+    model.phone = model.phone
+      .filter(a => a)
+      .map(p => ({
+        url: p.tel
+      }));
+    // Convert mobile Number
+    model.mobile = model.mobile
+      .filter(a => a)
+      .map(p => ({
+        url: p.tel
+      }));
+    // Convert skype
+    model.skype = model.skype
+      .filter(a => a)
+      .map(p => ({
+        url: p.text
+      }));
+
+    return model;
+  }
+
+  // Modify model from API to create form initial values
+  getInitialValues() {
+    const { model } = this.props;
+
+    if (!model) {
+      return {};
     }
 
-    // Convert form values to API model
-    createModelToSave(values) {
+    let v = {};
+    //Convert name for redux-form
+    v.name = model.name;
+    //Convert surname for redux-form
+    v.surname = model.surname;
+    //Convert gender for redux-form
+    v.gender = model.gender ? model.gender : "";
+    //Convert language preferred for redux-form
+    v.lang = model.lang ? model.lang : "";
+    //Convert birthday for redux-form
+    v.birthday = new Date(model.birthday);
+    //Convert citizenship for redux-form
+    v.citizenship = model.citizenship ? model.citizenship : "";
+    // Addresses_private: Add one item if value empty
+    v.addresses_private =
+      Array.isArray(model.addresses_private) &&
+      model.addresses_private.length > 0
+        ? model.addresses_private.map(a => ({
+            text: `${a.formatted_address}`
+          }))
+        : [{ text: "" }];
+    // Phone: Add one item if value empty
+    v.phone =
+      Array.isArray(model.phone) && model.phone.length > 0
+        ? model.phone.filter(a => a).map(p => ({ tel: p.url }))
+        : [""];
+    // Mobile: Add one item if value empty
+    v.mobile =
+      Array.isArray(model.mobile) && model.mobile.length > 0
+        ? model.phone.filter(a => a).map(p => ({ tel: p.url }))
+        : [""];
+    //Convert skype for redux-form
+    v.skype =
+      Array.isArray(model.skype) && model.skype.length > 0
+        ? model.skype.filter(a => a).map(p => ({ text: p.url }))
+        : [""];
 
-        //clone obj
-        let model = Object.assign({}, values);
+    return v;
+  }
 
-        //convert Gender
-        model.gender = values.gender.value;
-        //convert Lang
-        model.lang = values.lang.value;
-        //convert Birthday
-        model.birthday = moment(values.birthday).utc().format();
-        // Convert citizenship
-        model.citizenship = model.citizenship.filter(a => a).map(a => a.value);
-        // Convert addresses_private
-        model.addresses_private = model.addresses_private.map(a => {
-            const originalString = a.text;
-            const split = originalString.split(",");
-            const country = split[split.length - 1].trim();
-            const street = split[0].trim();
-            const locality = split[1].trim();
-            const formatted_address = originalString;
-            const geometry = a.geometry;
-            return {formatted_address, street, locality, country, geometry}
-        });
-        // Convert Phone Number
-        model.phone = model.phone.filter(a => a).map(p => ({
-            url: p.tel
-        }));
-        // Convert mobile Number
-        model.mobile = model.mobile.filter(a => a).map(p => ({
-            url: p.tel
-        }));
-        // Convert skype
-        model.skype = model.skype.filter(a => a).map(p => ({
-            url: p.text
-        }));
+  createLatLongToSave = address => {
+    return geocodeByAddress(address).then(results => getLatLng(results[0]));
+  };
 
-        return model;
-    }
+  onSubmit(values) {
+    const { showModal, saveModel, model } = this.props;
 
-    // Modify model from API to create form initial values
-    getInitialValues() {
-        const {model} = this.props;
+    let promises = [];
 
-        if (!model) {
-            return {};
+    const addrs = values.addresses_private;
+
+    addrs.forEach(a => {
+      promises.push(
+        this.createLatLongToSave(a.text).then(result => {
+          a.geometry = result;
+        })
+      );
+    });
+
+    return axios.all(promises).then(() => {
+      //dispatch the action to save the model here
+      const modelToSave = this.createModelToSave(values);
+
+      modelToSave._id = model._id;
+
+      return saveModel(modelToSave).then(response => {
+        if (response.model && response.model._id) {
+          showModal({
+            type: MODAL_SAVED
+          });
         }
+      });
+    });
+  }
 
-        let v = {};
-        //Convert name for redux-form
-        v.name = model.name;
-        //Convert surname for redux-form
-        v.surname = model.surname;
-        //Convert gender for redux-form
-        v.gender = model.gender ? model.gender : "";
-        //Convert language preferred for redux-form
-        v.lang = model.lang ? model.lang : "";
-        //Convert birthday for redux-form
-        v.birthday = model.birthday;
-        //Convert citizenship for redux-form
-        v.citizenship = model.citizenship ? model.citizenship : "";
-        // Addresses_private: Add one item if value empty
-        v.addresses_private = (Array.isArray(model.addresses_private) && model.addresses_private.length > 0) ?
-            model.addresses_private.map(a => ({
-                text: `${a.formatted_address}`
-            })) :
-            [{text: ""}];
-        // Phone: Add one item if value empty
-        v.phone = (Array.isArray(model.phone) && model.phone.length > 0) ?
-            model.phone.filter(a => a).map(p => ({tel: p.url})) : [""];
-        // Mobile: Add one item if value empty
-        v.mobile = (Array.isArray(model.mobile) && model.mobile.length > 0) ?
-            model.phone.filter(a => a).map(p => ({tel: p.url})) : [""];
-        //Convert skype for redux-form
-        v.skype = (Array.isArray(model.skype) && model.skype.length > 0) ?
-            model.skype.filter(a => a).map(p => ({text: p.url})) : [""];
+  render() {
+    const {
+      model = {},
+      countries,
+      showModal,
+      errorMessage,
+      isFetching
+    } = this.props;
 
-        return v;
-    }
+    return (
+      <div className="row">
+        <div className="col-md-2">
+          <LateralMenu />
+        </div>
+        <div className="col-md-10">
+          <h2 className="labelField">
+            <FormattedMessage
+              id="AccountPrivateData"
+              defaultMessage="My Account Private data"
+            />
+          </h2>
 
-    createLatLongToSave = (address) => {
-        return geocodeByAddress(address)
-        .then(results => getLatLng(results[0]))
+          <br />
 
-    }
+          {isFetching && !model && <Loading />}
 
-    onSubmit(values) {
-        const {showModal, saveModel, model} = this.props;
-        
-        let promises = []
+          {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
 
-        const addrs = values.addresses_private;
+          {!errorMessage && !isFetching && !model && <ItemNotFound />}
 
-        addrs.forEach(a => {
-            promises.push(this.createLatLongToSave(a.text).then((result) => {
-                 a.geometry = result;
-                }))
-        });
-
-        return axios.all(promises).then(() => {
-            //dispatch the action to save the model here
-            const modelToSave = this.createModelToSave(values);
-
-            modelToSave._id = model._id;
-
-            return saveModel(modelToSave)
-            .then((model) => {
-                if (model && model.id){
-                    showModal({
-                        type: MODAL_SAVED
-                    });
-                }
-            });
-        });
-
-    }
-    
-
-    render() {
-        const {model={}, countries, showModal, errorMessage, isFetching} = this.props;
-
-        return (
-            <div className="row">
-                <div className="col-md-2">
-                    <LateralMenu/>
-                </div>
-                <div className="col-md-10">
-                    <h2 className="labelField">
-                        <FormattedMessage
-                            id="AccountPrivateData"
-                            defaultMessage="My Account Private data"
-                        />
-                    </h2>
-
-                    <br/>
-
-                    {isFetching && !model && <Loading/>}
-
-                    {errorMessage && <ErrorMessage errorMessage={errorMessage}/>}
-
-                    {!errorMessage && !isFetching && !model && <ItemNotFound/>}
-
-                    <Form
-                        initialValues={this.getInitialValues()}
-                        onSubmit={this.onSubmit.bind(this)}
-                        showModal={showModal}
-                        countries={countries}
-                
-                    />
-                </div>
-            </div>
-        );
-    }
+          <Form
+            initialValues={this.getInitialValues()}
+            onSubmit={this.onSubmit.bind(this)}
+            showModal={showModal}
+            countries={countries}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 //Get form's initial values from redux state here
-const mapStateToProps = (state) => ({
-    model: getDefaultModel(state),
-    countries: getCountries(state),
-    isFetching: getDefaultModelIsFetching(state),
-    errorMessage: getDefaultModelErrorMessage(state),
+const mapStateToProps = state => ({
+  model: getDefaultModel(state),
+  countries: getCountries(state),
+  isFetching: getDefaultModelIsFetching(state),
+  errorMessage: getDefaultModelErrorMessage(state)
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    fetchModel,
-    saveModel,
-    showModal,
-    fetchCountries
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      fetchModel,
+      saveModel,
+      showModal,
+      fetchCountries
+    },
+    dispatch
+  );
 
 ProfilePrivate = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(ProfilePrivate);
 
 export default ProfilePrivate;
