322ea964769a0f09b4bd18d48b28a76464c9b577
Daniele Salvatore <danieleslvtr@gmail.com>
2019-01-16 19:59:21 +0100

    FIX refresh issue on Profile Emails

--- app/redux/components/galleries/selectors.js
+++ app/redux/components/galleries/selectors.js
@@ -21,7 +21,7 @@ export const getModel = (state, id) => state[entity].byId[id];
 
 export const getDefaultModel = state => {
   const firstId = Object.keys(state[entity].byId);
-  return firstId ? state[entity].byId[firstId] : null;
+  return firstId ? state[entity].byId[firstId] : undefined;
 };
 
 export const getModelErrorMessage = (state, id) =>
