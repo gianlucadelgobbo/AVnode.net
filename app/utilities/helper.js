9ca0e67fc75288b70a88079cf2f3827bfe403b61
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-01-18 23:06:47 +0100

    Link fix

--- app/utilities/helper.js
+++ app/utilities/helper.js
@@ -46,8 +46,8 @@ const linkify = (str) => {
   var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
   return str
-      .replace(urlPattern, '<a href="$&">$&</a>')
-      .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+      .replace(urlPattern, '<a href="$&" target="_blank">$&</a>')
+      .replace(pseudoUrlPattern, '$1<a href="http://$2" target="_blank">$2</a>')
       .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
 };
 
