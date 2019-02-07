ec286da163be5aed81a5c2970b5703ade85a1211
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-02-07 19:15:38 +0100

    small fix

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -34,16 +34,19 @@ function checkCookie() {
 }
 
 $(".cancel-sub").on('click', function(ev) {
-  const id = $(this).data("id");
-  console.log($(this).data("id"));
-  $.ajax({
-    url: "/admin/api/cancelsubscription",
-    method: "post",
-    data: {id:id}
-  }).done(function(data) {
-    console.log("#"+id);
-    $("#sub"+id).remove();
-  });
+  var result = confirm("Want to delete?");
+  if (result) {
+    const id = $(this).data("id");
+    console.log($(this).data("id"));
+    $.ajax({
+      url: "/admin/api/cancelsubscription",
+      method: "post",
+      data: {id:id}
+    }).done(function(data) {
+      console.log("#"+id);
+      $("#sub"+id).remove();
+    });
+  } 
 });
 
 $("#liker").on('click', function(ev) {
