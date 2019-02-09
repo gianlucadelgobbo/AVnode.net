a88cd36fe4dad690827a1b396325663b88a2f336
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-02-09 19:54:25 +0100

    loading bar

--- app/redux/components/galleries/gallery/constants.js
+++ app/redux/components/galleries/gallery/constants.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
 export const FORM_NAME = "GALLERY.GALLERIES";
+export const GALLERY_NAME =    <FormattedMessage
+                                id="crew.image"
+                                defaultMessage="GALLERY IMAGES"
+                            />
+export const SHOW = <FormattedMessage
+                        id="show"
+                        defaultMessage="SHOW"
+                    />
