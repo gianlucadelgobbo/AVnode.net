484a8eebd674fe3d9d31b9edcb9d65de3217d37c
piero80 <pcolangelo80@gmail.com>
2019-04-01 20:35:52 +0200

    Re-Adding Footage and Playlist Form

--- app/redux/components/topMenu/index.js
+++ app/redux/components/topMenu/index.js
@@ -1,22 +1,22 @@
-import React, {Component} from "react";
-import {FormattedMessage} from "react-intl";
-import {bindActionCreators} from "redux";
-import {NavLink} from "react-router-dom";
-import {connect} from "react-redux";
-import {fetchList as fetchCrews} from "../crews/actions";
-import {fetchList as fetchPerformances} from "../performances/actions";
-import {fetchList as fetchEvents} from "../events/actions";
-import {fetchList as fetchFootage} from "../footage/actions";
-import {fetchList as fetchPlaylists} from "../playlists/actions";
-import {fetchList as fetchVideos} from "../videos/actions";
-import {fetchList as fetchGalleries} from "../galleries/actions";
-import {getList as getCrews} from "../crews/selectors";
-import {getList as getPerformances} from "../performances/selectors";
-import {getList as getEvents} from "../events/selectors";
-import {getList as getFootage} from "../footage/selectors";
-import {getList as getPlaylists} from "../playlists/selectors";
-import {getList as getVideos} from "../videos/selectors";
-import {getList as getGalleries} from "../galleries/selectors";
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { bindActionCreators } from "redux";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchList as fetchCrews } from "../crews/actions";
+import { fetchList as fetchPerformances } from "../performances/actions";
+import { fetchList as fetchEvents } from "../events/actions";
+import { fetchList as fetchFootage } from "../footage/actions";
+import { fetchList as fetchPlaylists } from "../playlists/actions";
+import { fetchList as fetchVideos } from "../videos/actions";
+import { fetchList as fetchGalleries } from "../galleries/actions";
+import { getList as getCrews } from "../crews/selectors";
+import { getList as getPerformances } from "../performances/selectors";
+import { getList as getEvents } from "../events/selectors";
+import { getList as getFootage } from "../footage/selectors";
+import { getList as getPlaylists } from "../playlists/selectors";
+import { getList as getVideos } from "../videos/selectors";
+import { getList as getGalleries } from "../galleries/selectors";
 
 class TopMenu extends Component {
   componentDidMount() {
@@ -38,17 +38,17 @@ class TopMenu extends Component {
     fetchGalleries();
   }
 
-  createMenuItem = ({model, index}) => {
+  createMenuItem = ({ model, index }) => {
     return (
-        <NavLink
-            to={model.href}
-            activeClassName="active"
-            className="nav-link"
-            key={index}
-        >
-          {model.label}{" "}
-          <span className="badge badge-pill badge-info">{model.counter}</span>
-        </NavLink>
+      <NavLink
+        to={model.href}
+        activeClassName="active"
+        className="nav-link"
+        key={index}
+      >
+        {model.label}{" "}
+        <span className="badge badge-pill badge-info">{model.counter}</span>
+      </NavLink>
     );
   };
 
@@ -66,35 +66,35 @@ class TopMenu extends Component {
     const items = [
       {
         href: "/admin/profile/public",
-        label: <FormattedMessage id="profile" defaultMessage="Profile"/>
+        label: <FormattedMessage id="profile" defaultMessage="Profile" />
       },
       {
         href: "/admin/crews",
         counter: crews.length,
-        label: <FormattedMessage id="crews" defaultMessage="Crews"/>
+        label: <FormattedMessage id="crews" defaultMessage="Crews" />
       },
       {
         href: "/admin/performances",
         counter: performances.length,
         label: (
-            <FormattedMessage id="performances" defaultMessage="Performances"/>
+          <FormattedMessage id="performances" defaultMessage="Performances" />
         )
       },
       {
         href: "/admin/events",
         counter: events.length,
-        label: <FormattedMessage id="events" defaultMessage="Events"/>
+        label: <FormattedMessage id="events" defaultMessage="Events" />
       },
       {
         href: "/admin/videos",
         counter: videos.length,
-        label: <FormattedMessage id="videos" defaultMessage="Videos"/>
+        label: <FormattedMessage id="videos" defaultMessage="Videos" />
       },
       {
         href: "/admin/galleries",
         counter: galleries.length,
-        label: <FormattedMessage id="galleries" defaultMessage="Galleries"/>
-      }/* ,
+        label: <FormattedMessage id="galleries" defaultMessage="Galleries" />
+      },
       {
         href: "/admin/footage",
         counter: footage.length,
@@ -104,18 +104,22 @@ class TopMenu extends Component {
         href: "/admin/playlists",
         counter: playlists.length,
         label: <FormattedMessage id="playlists" defaultMessage="Playlists" />
-      } */
+      }
     ];
 
     return (
-        <nav id="account-nav" className="nav nav-pills nav-justified">
-          {items.map((model, index) => this.createMenuItem({model, index}))}
+      <nav id="account-nav" className="nav nav-pills nav-justified">
+        {items.map((model, index) => this.createMenuItem({ model, index }))}
 
-          <a className="nav-link" aria-current="false" href="/admin/subscriptions">
-            <span>Subscriptions</span>
-            <span className="badge badge-pill badge-info"></span>
-          </a>
-        </nav>
+        <a
+          className="nav-link"
+          aria-current="false"
+          href="/admin/subscriptions"
+        >
+          <span>Subscriptions</span>
+          <span className="badge badge-pill badge-info" />
+        </a>
+      </nav>
     );
   }
 }
@@ -131,22 +135,22 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators(
-        {
-          fetchCrews,
-          fetchPerformances,
-          fetchEvents,
-          fetchFootage,
-          fetchPlaylists,
-          fetchVideos,
-          fetchGalleries
-        },
-        dispatch
-    );
+  bindActionCreators(
+    {
+      fetchCrews,
+      fetchPerformances,
+      fetchEvents,
+      fetchFootage,
+      fetchPlaylists,
+      fetchVideos,
+      fetchGalleries
+    },
+    dispatch
+  );
 
 TopMenu = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(TopMenu);
 
 export default TopMenu;
