e73d0ff9e78eb9335fb98da266f136c36359e201
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-04-13 23:38:28 +0200

    cors

--- _run.sh
+++ _run.sh
@@ -1,13 +1,9 @@
 git fetch --all
 git reset --hard origin/master
-git reset --hard HEAD
+//git reset --hard HEAD
+//git clean -f -d
 git pull
 forever stop avnode.net
-npm run forever
+// npm run forever
 // OR
-// NODE_ENV=production forever --uid 'avnode.net' -l /sites/avnode.net/avnode-forever.log -o /sites/avnode.net/avnode-out.log -e /sites/avnode.net/avnode-err.log start --append index.js
-
-
-cd /sites/avnode.net/
-git checkout -f
-git pull
+NODE_ENV=production forever --uid 'avnode.net' -l /sites/avnode.net/avnode-forever.log -o /sites/avnode.net/avnode-out.log -e /sites/avnode.net/avnode-err.log start --append index.js
