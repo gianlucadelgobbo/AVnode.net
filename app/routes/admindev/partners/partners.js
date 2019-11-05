7aeb7e72a2d3850e5ff340588736d51c172bd41a
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-11-05 17:00:17 +0100

    organizationData

--- app/routes/admindev/partners/partners.js
+++ app/routes/admindev/partners/partners.js
@@ -174,6 +174,96 @@ router.get('/:id/:event', (req, res) => {
 
 router.get('/:id/:event/grantsdata', (req, res) => {
   logger.debug('/organizations/'+req.params.event);
+  var grantevents = {
+    flyer: [{
+      "Event Name": "Live Cinema Festival",
+      "Event Month": "September (second week)",
+      "Event City": "Rome",
+      "Event Country": "Italy",
+      "Description": "Live Cinema Festival aims to offer the audience an overview of different ways of interpreting Live Cinema, from analogic of projectors 16mm to the last digital experiments."
+    },{
+      "Event Name": "Fotonica Festival",
+      "Event Month": "December",
+      "Event City": "Rome",
+      "Event Country": "Italy",
+      "Description": "Fotonica is an Audio Visual Digital Art Festival that wants to explore the art forms related to the light element in the contemporary context."
+    },{
+      "Event Name": "LPM Live Performers Meeting",
+      "Event Month": "February",
+      "Event City": "DECIDED YEARLY BY THE COMMUNITY",
+      "Event Country": "",
+      "Description": "LPM Live Performers Meeting with 18 editions, is a nomade artists meeting open to the audience that offers the unique opportunity to experience 4 days of audiovisual performances, VJing, workshops, panel discussion, product showcases from over 400 artists from 42 countries with more than 250 show for every edition."
+    }],
+    telenoika: [{
+      "Event Name": "Visual Brasil",
+      "Event Month": "September (fourth week)",
+      "Event City": "Barcelona",
+      "Event Country": "Spain",
+      "Description": "The Visual Brasil Festival celebrates together with local and international artists a research meeting in the field of contemporary audiovisual: video art, mapping, audiovisual performances, workshops, installations and VJs. An activity that focuses on the production of video in real time, the culture of free creation and new collaborative formats."
+    }],
+    photon: [{
+      "Event Name": "Patchlab Festival",
+      "Event Month": "October (second week)",
+      "Event City": "Cracow",
+      "Event Country": "Poland",
+      "Description": "The International Digital Art Festival Patchlab is an annual event, which combines interdisciplinary digital works created on the border of arts, new technologies and creative programming."
+    }],
+    lunchmeat: [{
+      "Event Name": "Lunchmeat Festival",
+      "Event Month": "October (fourth week)",
+      "Event City": "Prague",
+      "Event Country": "Czech Republic",
+      "Description": "Lunchmeat is an annual international festival dedicated to advanced electronic music and new media art based in Prague, Czech Republic. It brings carefully selected creators from different art spheres together on one stage, creating a truly synesthetic experience."
+    }],
+    simultan: [{
+      "Event Name": "Simultan Festival",
+      "Event Month": "November (second week)",
+      "Event City": "Bucharest",
+      "Event Country": "Romania",
+      "Description": ""
+    }],
+    qvrtv: [{
+      "Event Name": "Thetaversal",
+      "Event Month": "November (fourth week)",
+      "Event City": "Galway",
+      "Event Country": "Ireland",
+      "Description": "ThetaVersal Audiovisual Festival will be a brand new event in Ireland with the aim to be a new platform for AV, VJs, Video and sound artists."
+    }],
+    jetztkultur: [{
+      "Event Name": "B-Seite Festival",
+      "Event Month": "March",
+      "Event City": "Mannheim",
+      "Event Country": "Germany",
+      "Description": "B-side Festival covers the entire spectrum audiovisual feeling of happiness from: Video Performance encounters musical delicacies, surprise interventions in public space and meet international artists in Mannheim scene icons."
+    }],
+    elasticeye: [{
+      "Event Name": "Splice Festival",
+      "Event Month": "April",
+      "Event City": "London",
+      "Event Country": "United Kingdom",
+      "Description": "Splice Festival, born in 2016 within AVnode 2015 > 2018 project, explores the overlapping fields of audio-visual art and culture through a collection of live performances, talks and workshops focused around live cinema, AV remixing, VJing, video art and projection mapping."
+    }],
+    multitrab: [{
+      "Event Name": "Athens Digital Art Festival",
+      "Event Month": "May",
+      "Event City": "Athens",
+      "Event Country": "Greece",
+      "Description": "Interactive works, audiovisual installations, video art, web art, digital image, creative workshops for adults and kids, artists’ talks, presentations of international festivals, AV performances and music events, highlighting the developments in new technologies and artistic practices."
+    }],
+    avmov: [{
+      "Event Name": "Amorphous Festival",
+      "Event Month": "June",
+      "Event City": "Caldas da Rainha",
+      "Event Country": "Portugal",
+      "Description": "Amorphous Festival wants to create an international AV event in Portugal, gathering national artists, both established and emerging ones, local students, programmers and teachers, at an event dedicated to audiovisual performance in Caldas da Rainha with workshops, Lectures, AV installations, micromapping, AV performances, vj/dj sets and live video mapping."
+    }],
+    avnode: [{
+      "Event Name": "AVnode Meeting",
+      "Event Month": "July",
+      "Event City": "Amsterdam",
+      "Event Country": "Netherlands",
+      "Description": "Annual meeting of the partners of the project in Amsterdam. 3 Days of full immersion on project development and report during the day and a selection of artists from every partner will follow in the evening."
+    }]};
   const query = {"partner_owner": req.params.id, "partnerships":req.params.event};
   logger.debug(query);
   User.
@@ -192,6 +282,7 @@ router.get('/:id/:event/grantsdata', (req, res) => {
         superuser:config.superusers.indexOf(req.user._id.toString())!==-1,
         owner: req.params.id,
         event: req.params.event,
+        grantevents: grantevents,
         user: req.user,
         printable: true,
         data: data,
