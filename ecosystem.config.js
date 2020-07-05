ae7c5e14abe751eed1f6755fea1ea0de8a4f41a0
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2020-07-05 23:48:47 +0200

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,39 +1,42 @@
 //pm2 deploy ecosystem.config.js production setup
 module.exports = {
   apps : [{
-    name: 'AVnode',
-    script: 'index.js',
-    error_file: 'logs/avnode-err.log',
-    out_file: 'logs/avnode-out.log',
-    log_file: 'logs/avnode-combined.log',
+    name: "avnode.net",
+    max_memory_restart: "600M",
+    script: "index.js",
+    log_file: "node_avnode-combined.log",
+    out_file: "node_avnode-out.log",
+    error_file: "node_avnode-err.log",
+    ignore_watch: [
+      "public",
+      "warehouse",
+      "glacier",
+      "locales"
+    ],
     time: true,
-  
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    //args: 'one two',
     instances: 1,
+    exec_mode: "fork",
     autorestart: true,
     watch: true,
-    ignore_watch: ["public","warehouse","glacier","locales","logs"],
     watch_options: {
-        followSymlinks: false
+      followSymlinks: false
     },
-    max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: "development"
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: "production"
     }
   }],
 
   deploy : {
     production : {
-      user : 'hyo',
-      host : [{host : '176.9.142.221',port : '9922'}],
-      ref  : 'origin/master',
-      repo : 'git@github.com:gianlucadelgobbo/AVnode.net.git',
-      path : '/sites/avnode.net/',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      user : "hyo",
+      host : [{host : "176.9.142.221",port : "9922"}],
+      ref  : "origin/master",
+      repo : "git@github.com:gianlucadelgobbo/AVnode.net.git",
+      path : "",
+      "post-deploy" : "npm install && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
