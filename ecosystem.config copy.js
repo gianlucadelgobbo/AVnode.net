4741a0352617d33426ddcc84590c15ad3b8bcc70
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2020-09-15 18:59:22 +0200

    small fix

--- ecosystem.config copy.js
+++ ecosystem.config copy.js
@@ -1,12 +1,12 @@
 //pm2 deploy ecosystem.config.js production setup
 module.exports = {
   apps : [{
-    name: "avnode.net en",
+    name: "avnode.net",
     max_memory_restart: "2G",
     script: "index.js",
-    log_file: "/sites/logs/node_en_avnode_net-combined.log",
-    out_file: "/sites/logs/node_en_avnode_net-out.log",
-    error_file: "/sites/logs/node_en_avnode_net-err.log",
+    log_file: "/sites/logs/node_avnode_net-combined.log",
+    out_file: "/sites/logs/node_avnode_net-out.log",
+    error_file: "/sites/logs/node_avnode_net-err.log",
     ignore_watch: [
       "public",
       "warehouse",
