fa5ea292828057fa092bbc7974f44d9ad970b84d
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2020-09-15 23:04:19 +0200

    fix

--- app/utilities/i18n.js
+++ app/utilities/i18n.js
@@ -3,6 +3,7 @@ const i18n = require('i18n');
 
 i18n.configure({
   locales:config.locales,
+  header: 'accept-language-disabled',
   defaultLocale: config.defaultLocale,
   directory: `${__dirname}/../../locales`,
   register: global
