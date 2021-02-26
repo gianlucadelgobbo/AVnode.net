a3751598fdcd530c624b76ecb9c44df22deb6f21
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2021-02-27 00:30:12 +0100

    console

--- app/utilities/image.js
+++ app/utilities/image.js
@@ -30,54 +30,41 @@ image.resize = (file, sizeA) => {
   var promise = new Promise((resolve, reject) => {
     logger.debug('resize');
     logger.debug(file);
-    //if (!file.err) {
-      const localFileName = file.path.substring(file.path.lastIndexOf('/') + 1); // file.path.jpg this.file.path.file.path.substr(19)
-      const localPath = file.path.substring(0, file.path.lastIndexOf('/')).replace('/glacier/', '/warehouse/').replace('_originals/', '/'); // /warehouse/2017/03
-      const localPathA = localPath.split('/');
-      let checkPath = '/';
-      for (let a=1; a<localPathA.length; a++) {
-        checkPath += localPathA[a]+'/';
-        logger.debug(checkPath);
-        if (!fs.existsSync(checkPath)) {
-          fs.mkdirSync(checkPath);
-        }
+    const localFileName = file.path.substring(file.path.lastIndexOf('/') + 1); // file.path.jpg this.file.path.file.path.substr(19)
+    const localPath = file.path.substring(0, file.path.lastIndexOf('/')).replace('/glacier/', '/warehouse/').replace('_originals/', '/'); // /warehouse/2017/03
+    const localPathA = localPath.split('/');
+    let checkPath = '/';
+    for (let a=1; a<localPathA.length; a++) {
+      checkPath += localPathA[a]+'/';
+      logger.debug(checkPath);
+      if (!fs.existsSync(checkPath)) {
+        fs.mkdirSync(checkPath);
       }
-      logger.debug('resize');
-      for (var a=0; a<sizeA.length; a++) {
-        var size = sizeA[a];
-        if (!fs.existsSync(`${localPath}/${size.folder}`)) {
-          fs.mkdirSync(`${localPath}/${size.folder}`);
-        }
-        const localFileNameWithoutExtension = localFileName.substring(0, localFileName.lastIndexOf('.'));
-        const localFileNameExtension = localFileName.substring(localFileName.lastIndexOf('.') + 1);
-        const scaledFilename = `${localPath}/${size.folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
-        sizeA[a].in = file.path;
-        sizeA[a].out = scaledFilename;
-        logger.debug('resize in  ' + file.path);
-        logger.debug('resize out ' + scaledFilename);
-        logger.debug(sizeA[a]);
+    }
+    logger.debug('resize');
+    for (var a=0; a<sizeA.length; a++) {
+      var size = sizeA[a];
+      if (!fs.existsSync(`${localPath}/${size.folder}`)) {
+        fs.mkdirSync(`${localPath}/${size.folder}`);
       }
-      const resize = size => sharp(size.in)
-      .resize(size.w, size.h)
-      .toFile(size.out);
-    
-      Promise
-      .all(sizeA.map(resize))
-      .then(() => {
-        console.log('complete');
-        setTimeout(resolve, 100, file);
-      });
-          /* sharp(file.path)
-      .resize(size.w, size.h)
-      .toFile(scaledFilename, (err, info) => {
-        if (err) {
-          file.err = err;
-        }
-        setTimeout(resolve, 100, file);
-        //
-      });   */
-
-    //}
+      const localFileNameWithoutExtension = localFileName.substring(0, localFileName.lastIndexOf('.'));
+      const localFileNameExtension = localFileName.substring(localFileName.lastIndexOf('.') + 1);
+      const scaledFilename = `${localPath}/${size.folder}/${localFileNameWithoutExtension}_${localFileNameExtension}.jpg`;
+      sizeA[a].in = file.path;
+      sizeA[a].out = scaledFilename;
+      logger.debug('resize in  ' + file.path);
+      logger.debug('resize out ' + scaledFilename);
+      logger.debug(sizeA[a]);
+    }
+    const resize = size => sharp(size.in)
+    .resize(size.w, size.h)
+    .toFile(size.out);
+  
+    Promise
+    .all(sizeA.map(resize))
+    .then(() => {
+      setTimeout(resolve, 100, file);
+    });
   });
   return promise
 };
