283d6d2cfc917d2ba6c69fb0209b0c97c2646b80
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2022-02-18 20:07:20 +0100

    fbgroups

--- public/js/admin_tmp.js
+++ public/js/admin_tmp.js
@@ -86,6 +86,12 @@ $(function () {
 		});
   });
 
+	$( ".getgroups button" ).click(function( event ) {
+		console.log(FB)
+		console.log($( ".getgroups input" ).val())
+		getGroups($( ".getgroups input" ).val())
+  });
+
 
 	$( ".categories_filter" ).click(function( event ) {
 		if ($( ".categories_filter" ).prop('checked')) {
