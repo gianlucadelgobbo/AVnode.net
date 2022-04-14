e904735d63a366842b810c1a26754d1549135f9e
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2022-04-14 22:45:59 +0200

    fix

--- app/routes/adminpro/events/events.js
+++ app/routes/adminpro/events/events.js
@@ -632,15 +632,17 @@ router.getMessageActs = (req, res) => {
         tosave.messages_tosend = [];
         tosave.messages_sent = [];
         data.program.forEach((item, index) => {
+          console.log("item")
+          console.log(item)
+          var message = {};
+          message.to_html = "";
+          message.cc_html = [];
+          message.from_name = req.body.from_name;
+          message.from_email = req.body.from_email;
+          message.user_email = req.body.user_email;
+          message.user_password = req.body.user_password;
+          message.subject = req.body.subject.split("[org_name]").join(item.performance.title);;
           if (item.subscriptions) {
-            var message = {};
-            message.to_html = "";
-            message.cc_html = [];
-            message.from_name = req.body.from_name;
-            message.from_email = req.body.from_email;
-            message.user_email = req.body.user_email;
-            message.user_password = req.body.user_password;
-            message.subject = req.body.subject.split("[org_name]").join(item.performance.title);;
             item.subscriptions.forEach((subscription, cindex) => {
               var contact = subscription.subscriber_id;
               if (contact) {
@@ -656,8 +658,8 @@ router.getMessageActs = (req, res) => {
                 }
               }
             });
-            if (message.to_html != "") tosave.messages_tosend.push(message)
           }
+          if (message.to_html != "") tosave.messages_tosend.push(message)
         });
         if (req.body.send == "1") {
           logger.debug(tosave);
